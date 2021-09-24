4ba0a3d5de5255ff0a8fd7fc86793e028adef030
Julien Alapetite <julien.alapetite@gmail.com>
2021-09-24 14:17:33 +0200

    add app

---
+++ opensankey/app.py
@@ -0,0 +1,13 @@
+from .server import create_app
+import flaskfilemanager
+import os
+
+app = create_app()
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+app.config['FLASKFILEMANAGER_FILE_PATH'] = os.path.join(parent_dir, 'users-folders')
+flaskfilemanager.init(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
