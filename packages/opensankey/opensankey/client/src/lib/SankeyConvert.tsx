c4f3bd70ae0c3b72c742d108e8d24e0154075009
Julien Alapetite <julien.alapetite@inria.fr>
2021-10-05 08:21:34 +0200

    fix convert

--- packages/opensankey/opensankey/client/src/lib/SankeyConvert.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyConvert.tsx
@@ -92,6 +92,9 @@ export const convert_data = (
   if (data.node_width===undefined) {
     data.node_width = 10
   }
+  if ( !data.tags) {
+    data.tags = []
+  }
 
   if (data.flux_types || data.use_flux_types) {
     if (data.tags.filter(tag => tag.tags_group_name === 'flux_types').length === 0) {
