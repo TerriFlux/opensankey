ad826c0eabe7f1d317e56304fc23454da9491cac
Julien Alapetite <julien.alapetite@gmail.com>
2021-11-01 16:15:29 +0100

    update export

--- packages/opensankey/opensankey/client/src/lib/index.tsx
+++ packages/opensankey/opensankey/client/src/lib/index.tsx
@@ -1,3 +1,6 @@
 
 import SankeyApp from './SankeyApp'
+import SankeyDraw from './SankeyDraw'
+import {convert_data} from './SankeyConvert'
 export default SankeyApp
+export {SankeyDraw,convert_data}
