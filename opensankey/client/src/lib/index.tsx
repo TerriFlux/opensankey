69f29cfa5964bf6cc2274cbc101913241060457e
Julien Alapetite <julien.alapetite@gmail.com>
2021-11-04 15:55:43 +0100

    maj version

--- opensankey/client/src/lib/index.tsx
+++ opensankey/client/src/lib/index.tsx
@@ -3,6 +3,7 @@ import SankeyApp from './SankeyApp'
 import SankeyDraw from './SankeyDraw'
 import {convert_data} from './SankeyConvert'
 import {compute_auto_sankey,updateLayout} from './SankeyLayout'
+import { nodeTooltipsContent, linkTooltipsContent } from './SankeyTooltip'
 
 export default SankeyApp
-export {SankeyDraw,convert_data,compute_auto_sankey,updateLayout}
+export {SankeyDraw,convert_data,compute_auto_sankey,updateLayout,nodeTooltipsContent,linkTooltipsContent}
