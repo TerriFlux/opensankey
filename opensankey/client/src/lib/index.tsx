f352d09287ffb59d0f6107c149d8fab15b5c2ef6
Julien Alapetite <julien.alapetite@gmail.com>
2021-11-04 15:26:44 +0100

    maj package

--- opensankey/client/src/lib/index.tsx
+++ opensankey/client/src/lib/index.tsx
@@ -2,5 +2,7 @@
 import SankeyApp from './SankeyApp'
 import SankeyDraw from './SankeyDraw'
 import {convert_data} from './SankeyConvert'
+import {compute_auto_sankey,updateLayout} from './SankeyLayout'
+
 export default SankeyApp
-export {SankeyDraw,convert_data}
+export {SankeyDraw,convert_data,compute_auto_sankey,updateLayout}
