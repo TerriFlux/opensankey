651065cdc51b05d0d13521c4105faef19f722e93
Julien Alapetite <julien.alapetite@gmail.com>
2021-12-18 10:11:43 +0100

    fix ven variable

--- opensankey/server/views.py
+++ opensankey/server/views.py
@@ -79,20 +79,7 @@ def upload_data():
 
 @opensankey.route('/sankey/upload_examples', methods=['POST'])
 def upload_exemple():
-    if not 'MFAData' in os.environ: 
-        response = Response(
-            response="{}",
-            status=405,
-            mimetype='application/json'
-        )
     data_folder = os.environ.get('MFAData')
-    # print('data_folder : '+data_folder)
-    response = Response(
-        response='data_folder : '+str(data_folder),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
     #exemples_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'exemples')
     exemple = request.get_data().decode("utf-8")
     exemple_file_path = os.path.join(data_folder, exemple)
