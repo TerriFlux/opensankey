22b4ac21e972ea589c5c956afd3d468996fb06e0
Julien Alapetite <julien.alapetite@inria.fr>
2022-01-19 19:10:45 +0100

    add save diagram

--- opensankey/server/parser_excel.py
+++ opensankey/server/parser_excel.py
@@ -368,113 +368,159 @@ def parse_simple_excel(
         'tags_catalog' : nodeTags
     }
 
-def updateLayout(
-  data,
-  new_layout
+def save_simple_excel(
+    sankey_data : dict
 ):
-  max_vertical_offset = 0
-  for node in data['nodes']:
-      if node['node_visible'] == 1:
-          max_vertical_offset = max(node['y'], max_vertical_offset)
-  max_vertical_offset = max_vertical_offset + 200
-  data['node_width'] = new_layout['node_width']
+    #mfa_input = io_excel.load_simple_excel(filepath)
 
-  # Apply nodes layout
-  for node_layout_key in new_layout['nodes']:
-    node_layout = new_layout['nodes'][node_layout_key]
-    nodes = [node for node in data['nodes'] if node['name'] == node_layout['name'] ]
-    if len(nodes) == 0:
-      if len(node_layout['inputLinksId']) == 0 and len(node_layout['outputLinksId']) == 0 and node_layout['shape_visible'] == False and node_layout['label_visible'] == True:
-        # Case of not a label
-        node = node_layout
-        node['idNode'] = 'node' + data['node_idx']
-        data['node_idx'] = data['node_idx'] + 1
-        data['nodes'][node['idNode']]
-      else:
-        continue
+    nodes_cols =  [
+        'Level', 'Element','Couleur', 'Forme'
+    ]
+    #nodes_cols = mfa_input['nodes'][0]
+    # tag_names are disposed between the column Dimensions and the column Définition
+    tag_names = sankey_data['dataTags'].keys() + sankey_data['tags_catalog'].keys()
+    nodes = {}
+    current_parent_level = 1
+    previous_level = 1
+    # nb_cols = len(nodes_cols)
+    # has_definition_col = False
+    # if nb_cols > len(base_cols):
+    #     for i in range(len(base_cols),len(nodes_cols)):
+    #        if nodes_cols[i] == 'Définition':
+    #            has_definition_col = True
+    #            break
+    #        tag_names.append(nodes_cols[i])
 
-    node = nodes[0]
-    if not node:
-      continue
-    if not node['node_visible']:
-      continue
-    node['name'] = node_layout['name']
-    node['x'] = node_layout['x']
-    node['y'] = node_layout['y']
-    if node['y'] + 200 > max_vertical_offset:
-      max_vertical_offset = node['y'] + 200
+    dataTags = {}
+    nodeTags = {}
+    #linkTags = {}
+    if len(tag_names) != 0:
+       tags = [[""] * 6] * len(tag_names)
+       tags[0] = ["Name","Type","Tags","isPalette","Colormap","Color"]
+       for i in range(tag_names):
+           if tag_names[i] in sankey_data['dataTags']:
+                tags[i+1][0] = tag_names[i]
+                tags[i+1][1] = 'dataTags'
+                tags[i+1][2] = [ tag['name'] for tag in sankey_data['dataTags'][tag_names[i]]['tags'].values()].join(':')
+           elif tag_names[i] in sankey_data['nodeTags']:
+                tags[i+1][0] = tag_names[i]
+                tags[i+1][1] = 'dataTags'
+                tags[i+1][2] = [ tag['name'] for tag in sankey_data['dataTags'][tag_names[i]]['tags'].values()].join(':')
 
-    #node.color = node_layout.color
-    node['x_label'] = node_layout['x_label']
-    node['y_label'] = node_layout['y_label']
-    node['label_visible'] = node_layout['label_visible']
+    nb_cols_nodes = 4 + len(sankey_data['tags_catalog'].keys())
+    nodes = [[""] * nb_cols_nodes] * len(sankey_data['nodes'].keys())
+    nodes[0] = ["Level","Element","Couleur","Forme"]+tag_names
 
-  # apply_input_outputLinksId(
-  #   new_layout['nodes,
-  #   new_layout['links,
-  #   data
-  # )
+    for node in sankey_data['nodes'].values():
+        nodes[i][nodes_cols.index('Element')] = node['name']
+        shape   = node['type']
+        if shape == 'sector' :
+            nodes[i][nodes_cols.index('Forme')] = 'rectangle' 
+        else:
+            nodes[i][nodes_cols.index('Forme')] = 'circle'
+        nodes[i][nodes_cols.index('Couleur')] = node['color']
 
-  for link_layout_key in new_layout['links']:
-    link_layout = new_layout['links'][link_layout_key]
-    links = [
-        link for link in data['links']
-        if data['nodes'][link['idSource']['name']] == new_layout['nodes'][link_layout['idSource']['name']] and
-           data['nodes'][link['idTarget']['name']] == new_layout['nodes'][link_layout['idTarget']['name']]
+        # color = mfa_input['nodes'][i][nodes_cols.index('Couleur')]
+        # if type(color) != str and math.isnan(color) or color == '':
+        #     color = 'grey'
+        # if not is_hex(color):
+        #     color = webcolors.name_to_hex(color)
+        if 'definition' in node:
+            nodes[i][nodes_cols.index('Définition')] = node['definition']             
+        # node_definition = None
+        # if has_definition_col and type(mfa_input['nodes'][i][nb_cols-1]) == str:
+        #     node_definition = mfa_input['nodes'][i][nb_cols-1]
+        #node_tags = {}
+        for i,tag_name in enumerate(tag_names):
+            nodes[i][5+i] = node[tag_name].join(':') 
+            # tag_value = mfa_input['nodes'][i][mfa_input['nodes'][0].index(tag_name)]
+            # if type(tag_value) != str and math.isnan(tag_value):
+            #     continue
+            # node_tags[tag_name] = tag_value.split(':')
+
+        nodes[i][nodes_cols.index('Level')] = node['dimensions']['Primaire']['level']
+        #new_node['dimensions']['Primaire']['level'] = int(level)
+        # parent_name = None
+        # if level > previous_level:
+        #     current_node_parent = nodes['node'+str(i-2)]
+        #     current_parent_level = previous_level
+        # if level > current_parent_level:
+        #     parent_name = current_node_parent['idNode']
+        # previous_level = level
+        # display = 1
+        # node_visible = 1
+        # if level != 1:
+        #   display = 0
+        #   node_visible = 0
+        # new_node = {
+        #     'idNode'        : 'node'+str(i-1),
+        #     'name'          : name,
+        #     'definition'    : node_definition,
+        #     'type'          : shape,
+        #     'tags'          : node_tags,
+        #     'dimensions'    : {
+        #         'Primaire':{
+        #             'parent_name': parent_name,
+        #             'level'      : int(level)
+        #         }
+        #     },
+        #     'label_visible' : 1,
+        #     'shape_visible' : 1,
+        #     'display'       : display,
+        #     'node_visible'  : node_visible,
+        #     'color'         : color
+        # }
+        # nodes[new_node['idNode']] = new_node
+
+    #flux_ws = pd.read_excel(excel_file, excel_file.sheet_names[1])
+
+    flux_cols = [
+        'Origin', 'Destination', 'Value'
     ]
 
-    if len(links) == 0:
-      continue
+    nb_cols_nodes = 3 + len(sankey_data['dataTags'].keys())
+    links = [[""] * nb_cols_nodes] * len(sankey_data['links'].keys())
+    #nb_tags = len(dataTags.keys())
+    for row,link in enumerate(sankey_data['links'].values()):
+        links[row][flux_cols.index('Origin')] = link['idSource']['name']
+        links[row][flux_cols.index('Destination')] = link['idTarget']['name']
+        # target_name =  mfa_input['flux_data'][row][flux_cols.index('Destination')]
+        # source_node = [nodes[key] for key in nodes.keys() if nodes[key]['name'] == source_name][0]
+        # target_node = [nodes[key] for key in nodes.keys() if nodes[key]['name'] == target_name][0] 
+        # if source_node['type'] == 'product':
+        #     color = source_node['color']
+        # elif target_node['type'] == 'product':
+        #     color = target_node['color']
+        # if not is_hex(color):
+        #   color = webcolors.name_to_hex(color)
+        link_tags= []
+        val = link['value']
+        for i,tag_name in enumerate(tag_names):
+            links[i][4+i] = link[tag_name].join(':')
+            val = val[tag_name]
+        links[row][flux_cols.index('Value')] = val        
+        # for i in range(nb_tags):
+        #     if len(mfa_input['flux_data'][row]) > 3+i:
+        #         link_tags.append(mfa_input['flux_data'][row][3+i])
 
-    link = links[0]
-    # if ( link_layout.display_value !== 'default' && 
-    #     !String(link_layout.display_value).includes('[') ) {
-    #   link.value = link_layout.value
-    # }
-    # const node_source = Object.values(data.nodes).filter( n => n.name ===new_layout['nodes[link_layout.idSource].name)
-    # const node_target = Object.values(data.nodes).filter( n => n.name ===new_layout['nodes[link_layout.idTarget].name)
-    # if (node_source && node_target) {
-    #   link.idSource = node_source.idNode
-    #   link.idSource = node_target.idNode
-    # }
-    #x_label, y_label, label_position, label_visible, recycling, curved, curvature, arrow,orthogonal_label_position = link_layout
-    link['curvature'] = link_layout['curvature']
-    link['curved'] = link_layout['curved']
-    link['arrow'] = link_layout['arrow']
-    link['text_color'] = link_layout['link_layout.text_color']
-    link['label_position'] = link_layout['label_position']
-    link['label_visible'] = link_layout['label_visible']
-    link['x_label'] = link_layout['x_label']
-    link['y_label'] = link_layout['y_label']
-    link['left_horiz_shift'] = link_layout['link_layout.left_horiz_shift']
-    link['right_horiz_shift'] = link_layout['link_layout.right_horiz_shift']
-    link['orientation'] = link_layout['link_layout.orientation']
-    link['recycling'] = link_layout['recycling']
-    link['orthogonal_label_position'] = link_layout['orthogonal_label_position']
-
-    # if (String(link['display_value[0]).includes('*')) {
-    #   link['value[0]'] = link_layout['link_layout.value[0]']
-    # }
-
-    if link_layout['vert_shift']:
-      link['left_horiz_shift'] = link_layout['link_layout.left_horiz_shift']
-      link['right_horiz_shift'] = link_layout['link_layout.right_horiz_shift']
-      link['vert_shift'] = link_layout['link_layout.vert_shift']
-
-  #data.animation_tooltips = new_layout['animation_tooltips
-  data['user_scale'] = new_layout['user_scale']
-  data['legend_position'] = new_layout['legend_position']
-  data['welcome_text'] = new_layout['welcome_text']
-  # if ('height' in new_layout) {
-  #   data.height = new_layout['height
-  # }
-  if 'width' in new_layout:
-    data['width'] = new_layout['width']
-
-#   Object.keys(new_layout['display_style).forEach(
-#     key => (data['display_style as any)[key] = (new_layout['display_style as any)[key]
-#   )
-  if 'filter' not in data['display_style']:
-    data['display_style']['filter'] = 0
-  if 'filter_label' not in data['display_style']:
-    data['display_style']['filter_label'] = 0
+        # existing_links = [links[key] for key in links.keys() if nodes[links[key]['idSource']]['name'] == source_name and nodes[links[key]['idTarget']]['name'] == target_name]
+        # if len(existing_links) > 0:
+        #     new_link = existing_links[0]
+        #     set_value(link_tags,0,new_link['value'], mfa_input['flux_data'][row][flux_cols.index('Value')],'default')
+        # else:
+        #     value = {}
+        #     set_value(link_tags,0,value, mfa_input['flux_data'][row][flux_cols.index('Value')],'default')
+        #     new_link = {
+        #         'idLink'   : 'link'+str(row-1),  
+        #         'idSource' : source_node['idNode'],
+        #         'idTarget' : target_node['idNode'],
+        #         'value'    : value,
+        #         'color'    : color
+        #     }
+        #     links[new_link['idLink']] = new_link
+    mfa_output = {
+        'nodes' : nodes,
+        'tags'  : tags,
+        'flux'  : links
+    }
+    return mfa_output
