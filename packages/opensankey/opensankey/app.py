3cb8a7f305113d9a2c7fa95cd326cde6a67099b5
Julien Alapetite <julien.alapetite@gmail.com>
2022-02-01 07:39:13 +0100

    fix path to data folder

--- packages/opensankey/opensankey/app.py
+++ packages/opensankey/opensankey/app.py
@@ -8,8 +8,9 @@ import os
 app = create_app()
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-app.config['FLASKFILEMANAGER_FILE_PATH'] = os.path.join(parent_dir, 'users-folders')
+mfa_data_dir = os.environ.get('MFAData')
+#parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+app.config['FLASKFILEMANAGER_FILE_PATH'] = os.path.join(mfa_data_dir)
 flaskfilemanager.init(app)
 
 if __name__ == "__main__":
