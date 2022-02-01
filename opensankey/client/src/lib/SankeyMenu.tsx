a95bd6688d55047d45eb77fdb66b9154b1fe3464
Julien Alapetite <julien.alapetite@gmail.com>
2022-02-01 07:40:24 +0100

    data location

--- opensankey/client/src/lib/SankeyMenu.tsx
+++ opensankey/client/src/lib/SankeyMenu.tsx
@@ -384,7 +384,7 @@ const Menu: FunctionComponent<MenuTypes> = (
               {edition_menu}
             </NavDropdown >
             <NavDropdown title="Exemples" id="exemples" >
-              <Dropdown.Item eventKey="data_repo" href="https://dev.terriflux.fr/fm/index.html" target="_blank">Données</Dropdown.Item>
+              <Dropdown.Item eventKey="data_repo" href="https://dev.open-sankey.fr/fm/index.html" target="_blank">Données</Dropdown.Item>
               <NavDropdown title="Pommes Poires" id="pommes_poires" >
                 <Dropdown.Item onClick={() => uploadExemple(
                   'SyntheticOpenSankey/pommes_poires_simple.xlsx', url_prefix, data, set_data, 
