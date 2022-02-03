05b105dda022d11cc87689dbc2352398c2d0a8ae
Julien Alapetite <julien.alapetite@gmail.com>
2022-02-03 07:27:11 +0100

    add dist2

--- opensankey/app.py
+++ opensankey/app.py
@@ -9,7 +9,6 @@ app = create_app()
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 mfa_data_dir = os.environ.get('MFAData')
-#parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 app.config['FLASKFILEMANAGER_FILE_PATH'] = os.path.join(mfa_data_dir)
 flaskfilemanager.init(app)
 
