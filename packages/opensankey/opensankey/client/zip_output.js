eb0c7c86e2c7d7fa0f03c584c75b82a8977226a9
Julien Alapetite <julien.alapetite@gmail.com>
2022-04-10 07:40:42 +0200

    add zip_output

---
+++ packages/opensankey/opensankey/client/zip_output.js
@@ -0,0 +1,23 @@
+var shell = require('shelljs')
+const zl = require("zip-lib");
+
+shell.cd('build')
+var files = []
+const zip = new zl.Zip();
+shell.ls().forEach(function (file) {
+  if (file.includes('reconciled.xlsx') || file.includes('pdf') ) {
+    zip.addFile(file)
+  }
+})
+
+// Adds a file from the file system
+// zip.addFile("path/to/file.txt", "renamedFile.txt");
+// zip.addFile("path/to/file2.txt", "folder/file.txt");
+// // Adds a folder from the file system, and naming it `new folder` within the archive
+// zip.addFolder("path/to/folder", "new folder");
+// Generate zip file.
+zip.archive(process.argv[2]+".zip").then(function () {
+    console.log("done");
+}, function (err) {
+    console.log(err);
+});
