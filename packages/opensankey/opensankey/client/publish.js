2cdd414e6e3284ba34c8da3a0ded497c2818c9b0
Julien Alapetite <julien.alapetite@gmail.com>
2022-07-16 13:20:53 +0200

    fix publish

--- packages/opensankey/opensankey/client/publish.js
+++ packages/opensankey/opensankey/client/publish.js
@@ -1,11 +1,10 @@
 var shell = require('shelljs')
-shell.echo('hello world')
 shell.cd('build')
 var j = 0
 shell.ls('*layout.json').forEach(function (file) {
   if (file.includes('auto_layout') ) {
     return
   }
-  shell.sed('-i', '^{', 'window.sankey.filiere'+j+' = {', file)
-  j=j+1
+  var files_root = file.split('_layout')[0]
+  shell.sed('-i', '^{', 'window.sankey.'+files_root+' = {', file)
 })
