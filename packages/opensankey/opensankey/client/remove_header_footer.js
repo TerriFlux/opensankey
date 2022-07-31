8c61a4cdaa73c15d096ae8d8076470528f88a769
Julien Alapetite <julien.alapetite@gmail.com>
2022-07-31 16:23:40 +0200

    move publish to mfadata

--- packages/opensankey/opensankey/client/remove_header_footer.js
+++
@@ -1,6 +0,0 @@
-var shell = require('shelljs')
-shell.cd('build')
-shell.ls('*.html').forEach(function (file,i) {
-  shell.sed('-i', 'has_header=!0' , 'has_header=0', file)
-  shell.sed('-i', 'footer=!0'     , 'footer=0', file)
-})
