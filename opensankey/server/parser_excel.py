420951d716c28149ca3e2d0c62ff5923778a39fa
Julien Alapetite <julien.alapetite@gmail.com>
2022-09-12 16:25:52 +0200

    fix dimensions

--- opensankey/server/parser_excel.py
+++ opensankey/server/parser_excel.py
@@ -466,7 +466,7 @@ def parse_nodes(mfa_input, nodes, nodeTags):
         level = mfa_input[NODES_SHEET][i][nodes_cols.index(NODES_LEVEL)]
         first_dimension = 'Primaire'
         if 'Dimensions' in nodeTags and 'Primaire' not in nodeTags['Dimensions']['tags']:
-            first_dimension = list(nodeTags['Dimensions']['tags'].keys()[0])
+            first_dimension = list(nodeTags['Dimensions']['tags'].keys())[0]
         dimension = 'Primaire'
         dimensions = ['Primaire']
         #if NODES_DIMENSIONS in mfa_input[NODES_SHEET][0]:
