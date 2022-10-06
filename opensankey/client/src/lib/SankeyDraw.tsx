3cfe580c4abcde945fa99bd2d181caae776ae980
Julien Alapetite <julien.alapetite@gmail.com>
2022-10-06 05:12:48 +0200

    fix #178

--- opensankey/client/src/lib/SankeyDraw.tsx
+++ opensankey/client/src/lib/SankeyDraw.tsx
@@ -4867,11 +4867,11 @@ const SankeyDraw: FunctionComponent<SankeyDrawTypes> = ({
           d3.select('#svg')
             .attr('transform', evt.transform).attr('transform-origin', '0 0')
           svgSankey.attr('viewBox', null)
-          //if (evt.transform.k < 1 && !data.fit_screen) {
-          //   d3.select('#svg')
-          //     .style('width', data.width / evt.transform.k + 'px')
-          //     .style('height', data.height / evt.transform.k + 'px')
-          // }
+          if (evt.transform.k < 1 && !data.fit_screen) {
+            d3.select('#svg')
+              .style('border', 2 / evt.transform.k + 'px solid #78c2ad')
+          }
+          
           // data.width=data.width / evt.transform.k
           // data.height=data.height/ evt.transform.k
           // drawGrid()
