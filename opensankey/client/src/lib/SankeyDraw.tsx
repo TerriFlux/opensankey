eede843bf7a6b2eba42c1bfbd6b13def7bc60f37
Julien Alapetite <julien.alapetite@inria.fr>
2022-10-06 10:19:52 +0200

    resolue

--- opensankey/client/src/lib/SankeyDraw.tsx
+++ opensankey/client/src/lib/SankeyDraw.tsx
@@ -1601,11 +1601,14 @@ const SankeyDraw: FunctionComponent<SankeyDrawTypes> = ({
         // })
         .call(d3.drag<SVGRectElement, unknown>()
           .subject(Object).on('drag', function (event) {
-            drag_handle(
-              link, nodes, links, display_style,
-              selected_tags,
-              this, position, event
-            )
+            if(multi_selected_links.current.includes(link)){
+              drag_handle(
+                link, nodes, links, display_style,
+                selected_tags,
+                this, position, event
+              )
+            }
+            
           })
         )
     }
