7b544bcaf8e2c64bd8949f5e012459dd3592d8c7
Julien Alapetite <julien.alapetite@gmail.com>
2022-10-20 16:45:34 +0200

    rename preversion

--- packages/opensankey/opensankey/client/src/lib/SankeyApp.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyApp.tsx
@@ -76,7 +76,7 @@ const SankeyApp: FunctionComponent<SankeyAppTypes> = ({ sankey_data, exemple_men
       <Menu
         data={data}
         set_data={set_data}
-        app_name={!window.SankeyToolsStatic ? 'preversion 1.0' : ''}
+        app_name={!window.SankeyToolsStatic ? 'Pré-version 1.0' : ''}
         set_current_filter={(
           new_current_filter: number
         ) => {
