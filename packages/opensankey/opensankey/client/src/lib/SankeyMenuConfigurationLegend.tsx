1126b60e99edd3615ff720bb4b885218c62ff0b9
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2023-03-27 07:56:04 +0000

    Resolve "Ajouter des tooltips pour les options du menu de configuration"

--- packages/opensankey/opensankey/client/src/lib/SankeyMenuConfigurationLegend.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyMenuConfigurationLegend.tsx
@@ -1,5 +1,5 @@
 import { SankeyData } from './types'
-import { Form, FormControl, FormLabel, Row, Col} from 'react-bootstrap'
+import { Form, FormControl, FormLabel, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import React from 'react'
 import { TFunction } from 'i18next'
 
@@ -11,55 +11,74 @@ export const OpenSankeyMenuConfigurationLegend= (
   legend_position:number[],
   set_legend_position:(n:number[])=>void
 ) => {
+
   return [
+    /* Position X de la legende  */
     <Form.Group as={Row} >
       <Col xs={3}>
         <FormLabel >{t('Menu.LegX')}</FormLabel>
       </Col>
       <Col>
-        <FormControl
-          type="text"
-          value={legend_position[0]}
-          onChange={evt => set_legend_position([+evt.target.value, legend_position[1]])}
-          onBlur={() => {
-            data.legend_position = legend_position
-            set_data({ ...data })
-          }}
-        />
+        <OverlayTrigger
+          key={'tooltip-adjust'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.LegX')} </Tooltip>}>
+            <FormControl
+              type="text"
+              value={legend_position[0]}
+              onChange={evt => set_legend_position([+evt.target.value, legend_position[1]])}
+              onBlur={() => {
+                data.legend_position = legend_position
+                set_data({ ...data })
+            }}/>
+        </OverlayTrigger>
       </Col>
     </Form.Group>,
+    /* Position Y de la legende */
     <Form.Group as={Row} >
       <Col xs={3}>
         <FormLabel>{t('Menu.LegY')}</FormLabel>
       </Col>
       <Col>
-        <FormControl
-          type="text"
-          value={legend_position[1]}
-          onChange={evt => set_legend_position([legend_position[0], +evt.target.value])}
-          onBlur={() => {
-            data.legend_position = legend_position
-            set_data({ ...data })
-          }}
-        />
+        <OverlayTrigger
+          key={'tooltip-adjust'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.LegY')} </Tooltip>}>
+            <FormControl
+              type="text"
+              value={legend_position[1]}
+              onChange={evt => set_legend_position([legend_position[0], +evt.target.value])}
+              onBlur={() => {
+                data.legend_position = legend_position
+                set_data({ ...data })
+            }}/>
+        </OverlayTrigger>
       </Col>
     </Form.Group>,
+    /* Largeur de la fenetre de legende */
     <Form.Group as={Row} >
       <Col xs={3}>
         <FormLabel>{t('Menu.LegWidth')}</FormLabel>
       </Col>
       <Col>
-        <FormControl
-          type="number"
-          step={1}
-          value={data.legend_width}
-          onChange={evt =>{
-            data.legend_width=+evt.target.value
-            set_data({ ...data })
-          }}
-        />
+      <OverlayTrigger
+          key={'tooltip-adjust'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.LegWidth')} </Tooltip>}>
+            <FormControl
+              type="number"
+              step={1}
+              value={data.legend_width}
+              onChange={evt =>{
+                data.legend_width=+evt.target.value
+                set_data({ ...data })
+            }}/>
+        </OverlayTrigger>
       </Col>
     </Form.Group>
   ]
-  
+
 }
--- packages/opensankey/opensankey/client/src/lib/SankeyMenuConfigurationLinksData.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyMenuConfigurationLinksData.tsx
@@ -1,5 +1,5 @@
-import React,{useState} from 'react'
-import { Row, Form, Col, FormLabel, FormCheck, Tab} from 'react-bootstrap'
+import React from 'react'
+import { Row, Form, Col, FormLabel, FormCheck, Tab, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { SankeyData, SankeyLink, SankeyLinkValue } from './types'
 import {value_selected_parameter} from './SankeyDrawFunction'
 import * as d3 from 'd3'
@@ -18,8 +18,8 @@ export const SankeyMenuConfigurationLinksData = (
   additional_data_element:JSX.Element[],
   displayed_value:string,
   set_displayed_value:(s:string)=>void
-
 )=>{
+
   const isAllLinkToPrecision=(multi_selected_links:{current:SankeyLink[]},)=>{
     let toPrecision = true
     multi_selected_links.current.map(d => {
@@ -27,7 +27,7 @@ export const SankeyMenuConfigurationLinksData = (
     })
     return toPrecision
   }
-  
+
   return <Tab eventKey="flux_data" title={t('Flux.data.données')}>
     <Form >
       {
@@ -41,19 +41,16 @@ export const SankeyMenuConfigurationLinksData = (
                     {dataTag.group_name} :
                   </FormLabel>
                 </Col>
-
                 <Col >
                   <Form.Select
                     name={dataTagKey}
                     value={tags_selected[dataTagKey]}
                     onChange={
                       (evt: React.ChangeEvent<HTMLSelectElement>) => {
-                      //Modifie les paramètres selectionnés 
+                      //Modifie les paramètres selectionnés
                         const { name, value } = evt.target
                         set_tags_selected(prevState => ({...prevState,[name]: value}))
-                      }
-                    }
-                  >
+                  }}>
                     {Object.entries(dataTag.tags).map(([tag_key, tag]) => {
                       return (
                         <option key={tag.name} value={tag_key}>{tag.name}</option>
@@ -64,113 +61,125 @@ export const SankeyMenuConfigurationLinksData = (
               </Row>
             )
           }
-
         })}
+
+      {/* Valeur du flux pour les parametre (flitres) choisi  */}
       <Col>
         <FormLabel>{t('Flux.data.vpp')}</FormLabel>
       </Col>
       <Col>
-        <Form.Control
-          className='inputValueLink'
-          type='text'
-          value={displayed_value}
-          onChange={
-            evt => {
-              set_displayed_value(evt.target.value)
-              const formatedValue=evt.target.value.replace(',','.')
-              if(formatedValue!='' && isNaN(+formatedValue)){
-                d3.select('.inputValueLink').style('border','red 1px solid')
-              }else{
-                d3.select('.inputValueLink').style('border','#ced4da 1px solid')
-              }
-            }
-          }
-          onBlur={evt=>{
-            const formatedValue=evt.target.value.replace(',','.')
-            if(formatedValue!=='' && !isNaN(+formatedValue )){
-              const was_empty=value_selected_parameter(data,multi_selected_links,tags_selected).value===''
-              let val = Object(selected_link.current.value)
-              multi_selected_links.current.map(d => {
-                d.dashed=(was_empty)?false:d.dashed
-                val = d.value
-                Object.values(tags_selected).forEach(tag => {
-                  if (val[tag] === undefined) {
-                    val[tag] = {}
+        <OverlayTrigger
+          key={'tooltip-adjust'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'tooltip-adjust'}>{t('Flux.data.tooltips.vpp')} </Tooltip>}>
+            <Form.Control
+              className='inputValueLink'
+              type='text'
+              value={displayed_value}
+              onChange={
+                evt => {
+                  set_displayed_value(evt.target.value)
+                  const formatedValue=evt.target.value.replace(',','.')
+                  if(formatedValue!='' && isNaN(+formatedValue)){
+                    d3.select('.inputValueLink').style('border','red 1px solid')
+                  }else{
+                    d3.select('.inputValueLink').style('border','#ced4da 1px solid')
                   }
-                  val = val[tag]
-                })
-                val.value = +formatedValue
-
-              })
-
-              const scale = d3.scaleLinear()
-                .domain([0, data.user_scale])
-                .range([0, 100])
-              if (scale(+formatedValue) > 500) {
-                data.user_scale = +formatedValue
+                }
               }
-            set_data({ ...data })
-
-            }else if(formatedValue==''){
-
-              let val = Object(selected_link.current.value)
-              multi_selected_links.current.map(d => {
-                val = d.value
-                d.dashed=true
-                Object.values(tags_selected).forEach(tag => {
-                  if (val[tag] === undefined) {
-                    val[tag] = {}
+              onBlur={evt=>{
+                const formatedValue=evt.target.value.replace(',','.')
+                if(formatedValue!=='' && !isNaN(+formatedValue )){
+                  const was_empty=value_selected_parameter(data,multi_selected_links,tags_selected).value===''
+                  let val = Object(selected_link.current.value)
+                  multi_selected_links.current.map(d => {
+                    d.dashed=(was_empty)?false:d.dashed
+                    val = d.value
+                    Object.values(tags_selected).forEach(tag => {
+                      if (val[tag] === undefined) {
+                        val[tag] = {}
+                      }
+                      val = val[tag]
+                    })
+                    val.value = +formatedValue
+                  })
+                  const scale = d3.scaleLinear()
+                    .domain([0, data.user_scale])
+                    .range([0, 100])
+                  if (scale(+formatedValue) > 500) {
+                    data.user_scale = +formatedValue
                   }
-                  val = val[tag]
-                })
-                val.value = ''
-
-              })
-            set_data({ ...data })
-            }
-          }}
-        />
+                  set_data({ ...data })
+                }
+                else if(formatedValue=='') {
+                  let val = Object(selected_link.current.value)
+                  multi_selected_links.current.map(d => {
+                    val = d.value
+                    d.dashed=true
+                    Object.values(tags_selected).forEach(tag => {
+                      if (val[tag] === undefined) {
+                        val[tag] = {}
+                      }
+                      val = val[tag]
+                    })
+                    val.value = ''
+                  })
+                set_data({ ...data })
+                }
+            }}/>
+        </OverlayTrigger>
       </Col>
+
+      {/* Choix d'affichage en notation scientifique  */}
       <Row>
         <Col>
           <Form.Label>{t('Flux.data.toPrecision')}</Form.Label>
         </Col>
         <Col>
-  
-          <FormCheck inline type='switch' checked={isAllLinkToPrecision(multi_selected_links)} onChange={evt=>{
-            Object.values(data.links).filter(f => multi_selected_links.current.map(d => d.idLink).includes(f.idLink)).map(d => d.to_precision = evt.target.checked)
-            set_data({...data})
-          }}></FormCheck>
+          <OverlayTrigger
+            key={'tooltip-adjust'}
+            placement={'top'}
+            delay={500}
+            overlay={<Tooltip id={'tooltip-adjust'}>{t('Flux.data.tooltips.toPrecision')} </Tooltip>}>
+              <FormCheck inline type='switch' checked={isAllLinkToPrecision(multi_selected_links)} onChange={evt=>{
+                Object.values(data.links).filter(f => multi_selected_links.current.map(d => d.idLink).includes(f.idLink)).map(d => d.to_precision = evt.target.checked)
+                set_data({...data})
+              }}/>
+          </OverlayTrigger>
         </Col>
       </Row>
 
-      
+      {/* Afficher ou non les donnée sur le Sankey  */}
       <Row >
         <Col>
           <FormLabel>{t('Flux.data.affichage')}</FormLabel>
         </Col>
         <Col>
-          <Form.Control
-            type='text'
-            value={value_selected_parameter(data,multi_selected_links,tags_selected).display_value}
-            onChange={
-              evt => {
-                let val = Object(selected_link.current.value)
-                multi_selected_links.current.map(d => {
-                  val = d.value
-                  Object.values(tags_selected).forEach(tag => {
-                    if (val[tag] === undefined) {
-                      val[tag] = {}
-                    }
-                    val = val[tag]
-                  })
-                  val.display_value = evt.target.value
-            
-                })
-                set_data({ ...data })
-              }
-            }
-          />
+        <OverlayTrigger
+            key={'tooltip-adjust'}
+            placement={'top'}
+            delay={500}
+            overlay={<Tooltip id={'tooltip-adjust'}>{t('Flux.data.tooltips.affichage')} </Tooltip>}>
+              <Form.Control
+                type='text'
+                value={value_selected_parameter(data,multi_selected_links,tags_selected).display_value}
+                onChange={
+                  evt => {
+                    let val = Object(selected_link.current.value)
+                    multi_selected_links.current.map(d => {
+                      val = d.value
+                      Object.values(tags_selected).forEach(tag => {
+                        if (val[tag] === undefined) {
+                          val[tag] = {}
+                        }
+                        val = val[tag]
+                      })
+                      val.display_value = evt.target.value
+                    })
+                    set_data({ ...data })
+              }}/>
+          </OverlayTrigger>
         </Col>
       </Row>
 
