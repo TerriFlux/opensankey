3b0562f8b76efc8d13dde711f2d998e5b4e6e127
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2023-03-28 12:16:14 +0000

    Resolve "Réorganiser arrangement des elements des menus / banner"

--- packages/opensankey/opensankey/client/src/lib/SankeyMenuConfigurationLayout.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyMenuConfigurationLayout.tsx
@@ -21,84 +21,38 @@ export const OpenSankeyMenuConfigurationLayout = (
   return [
     /* Couleur du fond de la page */
     <Form.Group as={Row}>
-      <Col xs={5}>
-        <FormLabel >{t('Menu.BgC')}</FormLabel>
+      <Col xs={7}>
+        <FormLabel>{t('Menu.BgC')}</FormLabel>
       </Col>
-      <Col xs={2}>
+      <Col xs={5}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.BgC'}
           placement={'top'}
           delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.BgC')} </Tooltip>}>
-          <Form.Control type='color' value={data.couleur_fond_sankey} onChange={evt=>{
-            // const c=evt.target.checkeds
-            data.couleur_fond_sankey=evt.target.value
-            set_data({...data})
+          overlay={<Tooltip id={'MEP.tooltips.BgC'}>{t('MEP.tooltips.BgC')} </Tooltip>}>
+          <Form.Control
+            type='color'
+            value={data.couleur_fond_sankey}
+            onChange={evt=>{
+              // const c=evt.target.checkeds
+              data.couleur_fond_sankey=evt.target.value
+              set_data({...data})
           }}/>
         </OverlayTrigger>
       </Col>
     </Form.Group>,
-    /* Echelle du Sankey */
+    /* Quadrillage */
     <Form.Group as={Row} >
-      <Col xs={3}>
-        <FormLabel >{t('MEP.Echelle')}</FormLabel>
-      </Col>
-      <Col>
-        <OverlayTrigger
-          key={'tooltip-adjust'}
-          placement={'top'}
-          delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.Echelle')} </Tooltip>}>
-          <FormControl
-            type="text"
-            value={user_scale}
-            onChange={evt => {
-              set_user_scale(+evt.target.value)
-            }}
-            onBlur={() => {
-              data.user_scale = user_scale
-              set_data({ ...data })
-            }}/>
-        </OverlayTrigger>
-        <FormControl.Feedback />
-        <Form.Text>({t('MEP.vp100')})</Form.Text>
-      </Col>
-    </Form.Group>,
-    /* Taille maximale du flux */
-    <Form.Group as={Row} >
-      <Col xs={3}>
-        <FormLabel >{t('MEP.MaxFlux')}</FormLabel>
-      </Col>
-      <Col>
-        <OverlayTrigger
-          key={'tooltip-adjust'}
-          placement={'top'}
-          delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.MaxFlux')} </Tooltip>}>
-          <FormControl
-            type="text"
-            value={maximum_flux == null ? undefined : maximum_flux}
-            onChange={evt => {
-              set_maximum_flux(+evt.target.value)
-            }}
-            onBlur={() => {
-              data.maximum_flux = maximum_flux
-              set_data({ ...data })
-            }}/>
-        </OverlayTrigger>
-      </Col>
-    </Form.Group>,
-    /* Taille de la grille */
-    <Form.Group as={Row} >
-      <Col xs={3}>
+      <Col xs={7}>
         <FormLabel >{t('MEP.TCG')}</FormLabel>
       </Col>
+      {/* Taille de la grille */}
       <Col xs={4}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.TCG'}
           placement={'top'}
           delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.TCG')} </Tooltip>}>
+          overlay={<Tooltip id={'MEP.tooltips.TCG'}>{t('MEP.tooltips.TCG')} </Tooltip>}>
           <FormControl
             type="number"
             min={1}
@@ -108,60 +62,86 @@ export const OpenSankeyMenuConfigurationLayout = (
             onChange={evt => {
               data.grid_square_size = (+evt.target.value >= 1) ? +evt.target.value : 10
               set_data({ ...data })
-            }}/>
+          }}/>
         </OverlayTrigger>
       </Col>
       {/* Afficher le quadrillage */}
-      <Col >
+      <Col xs={1}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.GV'}
           placement={'top'}
           delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.GV')} </Tooltip>}>
+          overlay={<Tooltip id={'MEP.tooltips.GV'}>{t('MEP.tooltips.GV')} </Tooltip>}>
           <FormCheck
             inline
             type='switch'
+            style={{marginRight: '0px', marginLeft: '-0.75em'}}
             checked={data.grid_visible}
-            label={t('MEP.GV')}
             onChange={() => {
               data.grid_visible = !data.grid_visible
               set_data({ ...data })
-            }}/>
+          }}/>
         </OverlayTrigger>
       </Col>
     </Form.Group>,
-    /* Arranger les noeud */
+    /* Echelle du Sankey */
     <Form.Group as={Row} >
-      <Col>
+      <Col xs={7}>
+        <FormLabel >{t('MEP.Echelle')}</FormLabel>
+      </Col>
+      <Col xs={5}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.Echelle'}
           placement={'top'}
           delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.AN')} </Tooltip>}>
-          <Button
-            size="sm"
-            onClick={() => {
-              arrangeNodes(data)
-              set_data({ ...data })
+          overlay={<Tooltip id={'MEP.tooltips.Echelle'}>{t('MEP.tooltips.Echelle')} </Tooltip>}>
+          <FormControl
+            type="text"
+            value={user_scale}
+            onChange={evt => {
+              set_user_scale(+evt.target.value)
             }}
-          >
-            {t('MEP.AN')}
-          </Button>
+            onBlur={() => {
+              data.user_scale = user_scale
+              set_data({ ...data })
+          }}/>
+        </OverlayTrigger>
+        <FormControl.Feedback />
+        <Form.Text>({t('MEP.vp100')})</Form.Text>
+      </Col>
+    </Form.Group>,
+    /* Taille maximale du flux */
+    <Form.Group as={Row} >
+      <Col xs={7}>
+        <FormLabel >{t('MEP.MaxFlux')}</FormLabel>
+      </Col>
+      <Col xs={5}>
+        <OverlayTrigger
+          key={'MEP.tooltips.MaxFlux'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'MEP.tooltips.MaxFlux'}>{t('MEP.tooltips.MaxFlux')} </Tooltip>}>
+          <FormControl
+            type="text"
+            value={maximum_flux == null ? undefined : maximum_flux}
+            onChange={evt => {
+              set_maximum_flux(+evt.target.value)
+            }}
+            onBlur={() => {
+              data.maximum_flux = maximum_flux
+              set_data({ ...data })
+          }}/>
         </OverlayTrigger>
       </Col>
     </Form.Group>,
-    /* Positionner automatiquement les noeuds */
+    /* Ecart horizontal  */
     <Form.Group as={Row} >
-      <Col xs={3}>
-        <FormLabel>{t('MEP.EEN')}</FormLabel>
-      </Col>
-      {/* Ecart horizontal  */}
-      <Col xs={2}>
+      <Col xs={7}>
         <FormLabel>{t('MEP.Horizontal')}</FormLabel>
       </Col>
-      <Col xs={2}>
+      <Col xs={5}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.EEN_h'}
           placement={'top'}
           delay={500}
           overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.EEN_h')} </Tooltip>}>
@@ -171,49 +151,67 @@ export const OpenSankeyMenuConfigurationLayout = (
             onChange={evt => {
               set_node_hspace(+evt.target.value)
               data.h_space = +evt.target.value
-            }}/>
+          }}/>
         </OverlayTrigger>
       </Col>
-      {/* Ecart Vertical */}
-      <Col xs={2}>
+    </Form.Group>,
+    /* Ecart Vertical */
+    <Form.Group as={Row}>
+      <Col xs={7}>
         <FormLabel>{t('MEP.Vertical')}</FormLabel>
       </Col>
-      <Col xs={2}>
+      <Col xs={5}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.EEN_v'}
           placement={'top'}
           delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.EEN_v')} </Tooltip>}>
+          overlay={<Tooltip id={'MEP.tooltips.EEN_v'}>{t('MEP.tooltips.EEN_v')} </Tooltip>}>
           <FormControl
             type="text"
             value={node_vspace}
             onChange={evt => {
               set_node_vspace(+evt.target.value)
               data.v_space = +evt.target.value
-            }}/>
+          }}/>
         </OverlayTrigger>
       </Col>
     </Form.Group>,
-    /* Mise en forme automatique */
-    <Form.Group as={Row} >
-      <Col xs={4}>
+    /* Positionnement des noeuds */
+    <Form.Group as={Row}>
+      { /* Mise en forme automatique */}
+      <Col xs={6}>
         <OverlayTrigger
-          key={'tooltip-adjust'}
+          key={'MEP.tooltips.PA'}
           placement={'top'}
           delay={500}
-          overlay={<Tooltip id={'tooltip-adjust'}>{t('Menu.tooltips.PA')} </Tooltip>}>
+          overlay={<Tooltip id={'MEP.tooltips.PA'}>{t('MEP.tooltips.PA')} </Tooltip>}>
           <Button
             size="sm"
             onClick={() => {
               compute_auto_sankey(data, node_hspace)
               set_data({ ...data })
-            }}
-          >
+          }}>
             {t('MEP.PA')}
+          </Button>
+        </OverlayTrigger>
+      </Col>
+      {/* Arranger les noeud */}
+      <Col xs={6}>
+        <OverlayTrigger
+          key={'MEP.tooltips.AN'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'MEP.tooltips.AN'}>{t('MEP.tooltips.AN')} </Tooltip>}>
+          <Button
+            size="sm"
+            onClick={() => {
+              arrangeNodes(data)
+              set_data({ ...data })
+          }}>
+            {t('MEP.AN')}
           </Button>
         </OverlayTrigger>
       </Col>
     </Form.Group>
   ]
 }
-
