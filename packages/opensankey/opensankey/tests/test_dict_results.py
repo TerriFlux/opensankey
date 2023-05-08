a3031c731a528d910a8b52d8a7f6466d49f41542
Julien Alapetite <julien.alapetite@gmail.com>
2023-05-08 06:34:07 +0200

    update tests

--- packages/opensankey/opensankey/tests/test_dict_results.py
+++ packages/opensankey/opensankey/tests/test_dict_results.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.getcwd())
 #     import sankeytools.server.parser_excel as parser_excel
 # except Exception:
 
-MAXSIZE = 100000
+MAXSIZE = 200000
 
 mfa_data_dir = os.environ.get('MFAData')
 expected_results = {}
