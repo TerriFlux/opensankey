2ab336e4f020faeeb7ab2cbcf9d58d2248d400a7
Vincent <vincent.clavel@terriflux.fr>
2023-05-30 16:14:52 +0200

    ajout images modèles dans carousel bienvenue

--- packages/opensankey/opensankey/client/src/lib/SankeyApp.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyApp.tsx
@@ -204,7 +204,7 @@ export const SankeyApp = ({initial_sankey_data,exemple_menu,formations_menu,logo
   const external_pagination=[pagination_intro]
   const external_content={'intro':intro} 
 
-  const intro_modal=!window.SankeyToolsStatic?OpenSankeyModalWelcome(t,active_page,set_active_page,show_modal_welcome,set_show_modal_welcome,never_see_again,set_never_see_again,[],external_pagination,external_content):<></>
+  const intro_modal=!window.SankeyToolsStatic?OpenSankeyModalWelcome(t,active_page,set_active_page,show_modal_welcome,set_show_modal_welcome,never_see_again,set_never_see_again,[],external_pagination,external_content,exemple_menu):<></>
 
 
   //- 1. Builds Configuration Menus
