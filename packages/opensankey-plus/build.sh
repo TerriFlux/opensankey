335e5b4abaf715ffaa01502474defd12732baa12
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2023-06-06 11:13:17 +0200

    Update CI script

---
+++ packages/opensankey-plus/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+exit_if_error() {
+  local exit_code=$1
+  shift
+  [[ $exit_code ]] &&               # do nothing if no error code passed
+    ((exit_code != 0)) && {         # do nothing if error code is 0
+      exit 1                        # we could also check to make sure error code is numeric when passed
+    }
+}
+
+# Front-end build
+cd client
+npm run lint || exit_if_error $?
+npm link --legacy-peer-deps open-sankey
+npm run build || exit_if_error $?
+cd ..
