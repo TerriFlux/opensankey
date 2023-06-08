3a2ed6c5ef1b60d6ccc31639a55fd03965630019
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2023-06-08 11:27:13 +0200

    Update build scripts

---
+++ build_client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exit_if_error() {
+  local exit_code=$1
+  shift
+  [[ $exit_code ]] &&               # do nothing if no error code passed
+    ((exit_code != 0)) && {         # do nothing if error code is 0
+      exit 1                        # we could also check to make sure error code is numeric when passed
+    }
+}
+
+# Front-end build
+cd opensankey/client
+npm install --legacy-peer-deps || exit_if_error $?
+npm link --legacy-peer-deps sankeyanimation || exit_if_error $?
+npm run lint || exit_if_error $?
+CI= npm run build || exit_if_error $?
+cd ../..
---
+++ build_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+exit_if_error() {
+  local exit_code=$1
+  shift
+  [[ $exit_code ]] &&               # do nothing if no error code passed
+    ((exit_code != 0)) && {         # do nothing if error code is 0
+      exit 1                        # we could also check to make sure error code is numeric when passed
+    }
+}
+
+# Install requirements
+pip install -r requirements.txt  || exit_if_error $?
+pip install -r conda_requirements.txt  || exit_if_error $?
+
+# Flake8
+cd opensankey/server
+flake8  || exit_if_error $?
+cd ../..
+
+# Install
+pip install .  || exit_if_error $?
