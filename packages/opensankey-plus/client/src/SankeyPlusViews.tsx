413dc332312b17ace2d2b391a85704f4eb9eede9
Julien Alapetite <julien.alapetite@gmail.com>
2023-06-09 18:51:43 +0200

    fix update views

--- packages/opensankey-plus/client/src/SankeyPlusViews.tsx
+++ packages/opensankey-plus/client/src/SankeyPlusViews.tsx
@@ -1091,12 +1091,9 @@ export const get_data_from_view=(master_data:SankeyPlusData,id_view_to_see:strin
       master_data.links[d.rhs] == undefined)
 
   // Apply the changements saved in the view to the copy of master then return 'master data + modification saved in the view'
-  //diff_view.filter((d : {path:string[],kind:string})=>(d.path[0] !== 'links' || d.kind !== 'D')).forEach((d : object)=>applyChange(data_init,{},d))
-  //diff_view.forEach((d : object)=>applyChange(data_init,{},d))
-  //diff_view.forEach((d : object)=>applyChange(data_init,{},d))
   diff_view
-  .filter((d :{path:string[],kind:string}) => d.kind === 'N' || d.path[0] !== 'nodes' || master_data.nodes[d.path[1]] !== undefined)
-  .filter((d :{path:string[],kind:string}) => d.kind === 'N' || d.path[0] !== 'links' || master_data.links[d.path[1]] !== undefined)
+  .filter((d :{path:string[],kind:string}) => (d.kind === 'N' && d.path.length==2) || d.path[0] !== 'nodes' || master_data.nodes[d.path[1]] !== undefined)
+  .filter((d :{path:string[],kind:string}) => (d.kind === 'N' && d.path.length==2) || d.path[0] !== 'links' || master_data.links[d.path[1]] !== undefined)
   .filter((d :{path:string[],kind:string}) => (d.path[0] !== 'links' || d.kind !== 'D'))
   .forEach((d :{path:string[],kind:string}) => applyChange(data_init, {}, d));
   if (ignore_changes.length > 0 || ignore_changes2.length > 0 || ignore_changes3.length > 0) {
