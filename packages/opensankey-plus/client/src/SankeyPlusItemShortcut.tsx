c17a3ee6c744919da8df5c9bc64ca7ae69d3587a
Vincent <vincent.clavel@terriflux.fr>
2023-06-29 17:03:18 +0200

    ajout raccourcie

--- packages/opensankey-plus/client/src/SankeyPlusItemShortcut.tsx
+++ packages/opensankey-plus/client/src/SankeyPlusItemShortcut.tsx
@@ -7,6 +7,9 @@ export const SankeyPlusShortcut=(t:TFunction)=>{
     <h4 style={{textAlign:'center'}}>{t('Menu.rcc_titre_OSP')} :</h4>
     <p><b>{t('Menu.rcc_osp_cs_bold')}</b>{t('Menu.rcc_osp_cs')}</p>
     <p><b>{t('Menu.rcc_osp_ctrl_czdt_bold')}</b>{t('Menu.rcc_osp_ctrl_czdt')}</p>
+    <p><b>{t('Menu.rcc_F7_bold')}</b>{t('Menu.rcc_F7')}</p>
+    <p><b>{t('Menu.rcc_F8_bold')}</b>{t('Menu.rcc_F8')}</p>
+    <p><b>{t('Menu.rcc_F9_bold')}</b>{t('Menu.rcc_F9')}</p>
   </React.Fragment>
   )
 }
