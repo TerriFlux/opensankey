3fb25ce97ec7e807b1ae07ce24459998694a4871
Vincent <vincent.clavel@terriflux.fr>
2023-06-29 12:01:09 +0200

    Shortcut trad

--- packages/opensankey-plus/client/src/SankeyPlusItemShortcut.tsx
+++ packages/opensankey-plus/client/src/SankeyPlusItemShortcut.tsx
@@ -1,11 +1,12 @@
 
+import { TFunction } from 'i18next'
 import React from 'react'
 
-export const SankeyPlusShortcut=()=>{
+export const SankeyPlusShortcut=(t:TFunction)=>{
   return (<React.Fragment>
-    <h4  style={{textAlign:'center'}}>Raccourcis du module OpenSankey+</h4>
-    <p><b>CTRL+S :</b>Sauvegarde la configuration actuelle dans une vue, qui peut ensuite être visualisé dans le Menu Vue </p>
-    <p><b>CTRL+Click (Zone Texte) :</b>Sélectionne la zone clické et ouvre le menu d'édition de zone de texte </p>
+    <h4 style={{textAlign:'center'}}>{t('Menu.rcc_titre_OSP')} :</h4>
+    <p><b>{t('Menu.rcc_osp_cs_bold')}</b>{t('Menu.rcc_osp_cs')}</p>
+    <p><b>{t('Menu.rcc_osp_ctrl_czdt_bold')}</b>{t('Menu.rcc_osp_ctrl_czdt')}</p>
   </React.Fragment>
   )
 }
