43a54e7882770b804aeda18795bece35e3ba231a
Vincent <vincent.clavel@terriflux.fr>
2023-07-11 15:45:14 +0200

    correction lint

--- packages/opensankey-plus/client/src/SankeyPlusUtils.tsx
+++ packages/opensankey-plus/client/src/SankeyPlusUtils.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { Row, Form, FormLabel, Col, FormCheck, Tab, Button } from 'react-bootstrap'
-import { SankeyPlusData, SankeyPlusNode,SankeyPlusLinkStyle } from './types'
-import { reorganize_node_inputLinksId,reorganize_node_outputLinksId } from 'open-sankey/dist/SankeyLayout'
-import { default_link,default_link_style } from 'open-sankey/dist/SankeyUtils'
+
+import { SankeyPlusLinkStyle } from './types'
+import { default_link_style } from 'open-sankey/dist/SankeyUtils'
 
 export const default_sankey_plus_style_link=()=>{
-    const style=default_link_style() as SankeyPlusLinkStyle
-    style.gradient=false
-    return style
+  const style=default_link_style() as SankeyPlusLinkStyle
+  style.gradient=false
+  return style
 }
