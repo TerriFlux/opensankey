f0a96b7dd0ca4879229d3c1555ff1ff20678dc0a
Vincent <vincent.clavel@terriflux.fr>
2023-07-17 14:26:24 +0200

    label noeud editable sur la zdd

--- packages/opensankey/opensankey/client/src/lib/SankeyDrawNodesLabel.tsx
+++ packages/opensankey/opensankey/client/src/lib/SankeyDrawNodesLabel.tsx
@@ -1,20 +1,56 @@
 import { SankeyData, SankeyNode,SankeyLinkValue } from './types'
 import * as d3 from 'd3'
 
-import { dragNodeTextEventWidthBoxEvent } from './SankeyDrag'
+import { dragNodeTextEventWidthBoxEvent} from './SankeyDrag'
 import {textNodeValue,node_label_posX,node_label_posY,node_value_posX,node_value_posY,node_label_text,textNodeWrap} from './SankeyDrawFunction'
 import { node_displayed,return_value_node } from './SankeyUtils'
 
-
+declare const window: Window &
+typeof globalThis & {
+  SankeyToolsStatic: boolean
+  sankey: {
+    sankey_data_file:RequestInfo
+    sous_filieres : { [ key : string ] : string }
+    units: string[]
+    flask_logo? : string
+    flask_header? : string
+    logo_width? : number
+    legend_average : string
+    legend_uncert : string
+    help_text : string
+    welcome_text: string
+    excel : string
+    logo: string,
+    advanced: boolean
+  }
+}
 export const OpenSankeyDrawNodesLabel = (
   data:SankeyData, 
   set_data:(d:SankeyData)=>void,
   multi_selected_nodes:{current: SankeyNode[] },
-  getLinkValue:(data: SankeyData, idLink: string, up?: boolean) => SankeyLinkValue
+  getLinkValue:(data: SankeyData, idLink: string, up?: boolean) => SankeyLinkValue,
+  accept_simple_click:{current:boolean}
   
 ) => {
   const display_nodes=data.nodes
   const display_links=data.links
+
+  const DoubleGNodeClick=(event:React.MouseEvent<HTMLButtonElement>,d:SankeyNode)=>{
+    accept_simple_click.current=false
+    
+    const label_x=document.getElementById(d.idNode+'_text')?.getBoundingClientRect().x??0
+    const label_y=document.getElementById(d.idNode+'_text')?.getBoundingClientRect().y??0
+    const node_x=document.getElementById(d.idNode)?.getBoundingClientRect().x??0
+    const node_y=document.getElementById(d.idNode)?.getBoundingClientRect().y??0
+    
+    d3.select('#'+d.idNode+'_fo_input_label').style('display','inline-block')
+    d3.select('#'+d.idNode+'_fo_input_label').attr('x',(label_x-node_x)).attr('y',label_y-node_y)
+    d3.select('#'+d.idNode+'_text').style('display','none')
+    document.getElementById(d.idNode+'_input_label')?.focus()
+    setTimeout(()=>{
+      accept_simple_click.current=true
+    },200)
+  }
 
   const add_nodes_label = (
   ) => {
@@ -22,7 +58,7 @@ export const OpenSankeyDrawNodesLabel = (
     //------------------LABEL------------------------
     // Add node label and apply parameter
     const ggg_nodes=(d3.selectAll('.ggg_nodes') as d3.Selection<SVGGElement, SankeyNode, d3.BaseType, unknown>)
-    ggg_nodes
+    const text_node=ggg_nodes
       .append('text')
       .attr('fill',n=>(return_value_node(data,n,'label_color') )?'white':'black')
       .classed('node', true)
@@ -51,6 +87,37 @@ export const OpenSankeyDrawNodesLabel = (
       .style('text-transform', n => (return_value_node(data,n,'uppercase')) ? 'uppercase' : 'none')
       .text(n => node_label_text(data,n as SankeyNode))
       .each(n => textNodeWrap((n as SankeyNode),data))
+    
+    if(!window.SankeyToolsStatic){
+      // Add an input to change the name of the node 
+      // The input appear when we double click on the label
+      (d3.selectAll('.ggg_nodes') as d3.Selection<SVGGElement, SankeyNode, d3.BaseType, unknown>)
+        .append('foreignObject')
+        .attr('id',d=>d.idNode+'_fo_input_label')
+        .attr('x',(n)=>node_label_posX(data,n as SankeyNode))
+        .attr('y',(n)=>node_label_posY(n as SankeyNode,data))
+        .attr('width',d=>Number(return_value_node(data,d,'label_box_width'))+5)
+        .attr('height',d=>Number(return_value_node(data,d,'font_size'))+2)
+        // .attr('position','fixed')
+        .style('display','none')
+        .append('xhtml:div')
+        .append('input')
+        .attr('id',d=>d.idNode+'_input_label')
+        .attr('class','input_label')
+        .attr('type','text')
+        .attr('value',d=>d.name)
+        .style('font-size', n => return_value_node(data,n,'font_size') + 'px')
+        .on('input',(evt,d)=>d.name=evt.target.value)
+        .style('background-color','white')
+        .style('border','none')
+        .style('border-color','transparent')
+        .style('width',d=>Number(return_value_node(data,d,'label_box_width'))+'px')
+        .style('height',d=>Number(return_value_node(data,d,'font_size'))+'px')
+        .style('outline','none')
+        
+      text_node.on('dblclick',(event, d)=>DoubleGNodeClick(event,d))
+
+    }
 
     // Display value of nodes
     // Value of nodes are the maximum between the sum of input links and the sum of output links
