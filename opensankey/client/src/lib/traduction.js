8d4678ab44d430f43be8dc7d50f1e75bc3782c51
Vincent <vincent.clavel@terriflux.fr>
2023-08-10 12:11:18 +0200

    random link color context_zdd

--- opensankey/client/src/lib/traduction.js
+++ opensankey/client/src/lib/traduction.js
@@ -137,6 +137,7 @@ const resources = {
         'rth_support':'We are here to help you',
         'support_explication':'If you encounter a technical problem, write to us at [] and we will deal with your request as quickly as possible.',
         'suppr':'Delete',
+        'rand_link_color':'Assign random color to all links',
         tooltips:{
           noeud:{
             slct: 'Choose one / some / all nodes to select via a drop-down list',
@@ -685,6 +686,8 @@ const resources = {
         'rth_support':'Nous sommes là pour vous aidez',
         'support_explication':'Si vous rencontrez un problème technique, écrivez-nous à [], nous traiterons votre demande dans les meilleurs délais.',
         'suppr':'Supprimer',
+        'rand_link_color':'Assigne une couleur aléatoire à tous les flux',
+
         tooltips:{
           noeud:{
             slct: 'Choisir un / des / tous les noeud(s) à sélectionner via une liste déroulante',
