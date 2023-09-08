fc81fc8bf6927b197c282f2f0f5bfa7ec2f1ed1f
Vincent <vincent.clavel@terriflux.fr>
2023-09-08 17:09:31 +0200

    Raccourci en accordeon dans bienvenue

--- packages/opensankey-plus/client/src/SankeyPlusItemShortcut.tsx
+++ packages/opensankey-plus/client/src/SankeyPlusItemShortcut.tsx
@@ -1,15 +1,23 @@
 
 import { TFunction } from 'i18next'
 import React from 'react'
+import { Accordion } from 'react-bootstrap'
 
 export const SankeyPlusShortcut=(t:TFunction)=>{
-  return (<React.Fragment>
-    <h4 style={{textAlign:'center'}}>{t('Menu.rcc_titre_OSP')} :</h4>
-    <p><b>{t('Menu.rcc_osp_cs_bold')}</b>{t('Menu.rcc_osp_cs')}</p>
-    <p><b>{t('Menu.rcc_osp_ctrl_czdt_bold')}</b>{t('Menu.rcc_osp_ctrl_czdt')}</p>
-    <p><b>{t('Menu.rcc_F7_bold')}</b>{t('Menu.rcc_F7')}</p>
-    <p><b>{t('Menu.rcc_F8_bold')}</b>{t('Menu.rcc_F8')}</p>
-    <p><b>{t('Menu.rcc_F9_bold')}</b>{t('Menu.rcc_F9')}</p>
-  </React.Fragment>
+  return (<Accordion>
+    <Accordion.Item eventKey='OSP'>
+      <Accordion.Header>
+        <h2>{t('Menu.rcc_titre_OSP')} :</h2>
+
+      </Accordion.Header>
+      <Accordion.Body>
+        <p><b>{t('Menu.rcc_osp_cs_bold')}</b>{t('Menu.rcc_osp_cs')}</p>
+        <p><b>{t('Menu.rcc_osp_ctrl_czdt_bold')}</b>{t('Menu.rcc_osp_ctrl_czdt')}</p>
+        <p><b>{t('Menu.rcc_F7_bold')}</b>{t('Menu.rcc_F7')}</p>
+        <p><b>{t('Menu.rcc_F8_bold')}</b>{t('Menu.rcc_F8')}</p>
+        <p><b>{t('Menu.rcc_F9_bold')}</b>{t('Menu.rcc_F9')}</p>
+      </Accordion.Body>
+    </Accordion.Item>
+  </Accordion>
   )
 }
