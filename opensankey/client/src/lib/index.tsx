07f1a01702ead31430aa7a8f77fc1346b4180be6
Vincent <vincent.clavel@terriflux.fr>
2023-12-22 15:39:58 +0100

    add type function

--- opensankey/client/src/lib/index.tsx
+++ opensankey/client/src/lib/index.tsx
@@ -2,8 +2,9 @@
 import SankeyApp from './SankeyApp'
 import SankeyDraw from './SankeyDraw'
 import {convert_data} from './SankeyConvert'
-import {compute_auto_sankey,updateLayout} from './SankeyLayout'
+import {ComputeAutoSankey} from './SankeyLayout'
+import { updateLayout } from './SankeyUtils'
 import { NodeTooltipsContent, LinkTooltipsContent } from './SankeyTooltip'
 
 export default SankeyApp
-export {SankeyDraw,convert_data,compute_auto_sankey,updateLayout,NodeTooltipsContent,LinkTooltipsContent}
+export {SankeyDraw,convert_data,ComputeAutoSankey,updateLayout,NodeTooltipsContent,LinkTooltipsContent}
