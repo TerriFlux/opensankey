a869c81fd0863d8a18840c3118ce5ef21566abeb
Vincent <vincent.clavel@terriflux.fr>
2024-01-02 12:12:34 +0100

    modif pour su-model/sankeysuitemanager#491

--- opensankey/client/src/types/SankeyDrawLinksTypes.tsx
+++ opensankey/client/src/types/SankeyDrawLinksTypes.tsx
@@ -1,34 +1,26 @@
 import { RefObject } from 'react'
 import { drawArrowsType } from './SankeyDrawFunctionTypes'
 import { GetLinkValueFuncType, LinkColorFuncType, LinkTextFuncType } from './SankeyUtilsTypes'
-import { SankeyData, SankeyLink, SankeyNode } from './Types'
+import { SankeyData, SankeyLink, SankeyNode, applicationDataType, contextMenuType, elementsSelectedType, uiElementsRefType } from './Types'
 
 export type LinkStrokeFuncType = (l: SankeyLink, data: SankeyData, GetLinkValue: GetLinkValueFuncType) => string
 
-export type OpenSankeyDrawLinksFType = (
-  data:SankeyData,
-  display_nodes:{ [node_id: string]: SankeyNode },
-  display_links:{ [link_id: string]: SankeyLink },
-  links_accordion_ref:{ current: HTMLDivElement } | null,
-  multi_selected_links:{current: SankeyLink[] },
+export type DrawLinksFType = (
+  contextMenu:contextMenuType,
+  applicationData:applicationDataType,
+  uiElementsRef:uiElementsRefType,
+  elementsSelected:elementsSelectedType,
   mode_selection:{current:string},
-  accordion_ref:{ current: HTMLDivElement } | null,
-  button_ref:{ current: HTMLLabelElement} | null,
   alt_key_pressed:boolean,
   position:'absolute' | 'relative',
   node_arrow_visible:(data:SankeyData,n: SankeyNode)=>boolean,
   LinkTooltipsContent:(data: SankeyData, l: SankeyLink, GetLinkValue:GetLinkValueFuncType) => string,
   LinkText:LinkTextFuncType,
   GetLinkValue:GetLinkValueFuncType,
-  set_data:(d:SankeyData)=>void,
   displayedInputLinkValueRef: RefObject<HTMLInputElement>,
-  tags_selected:{[k: string]: string},
-  set_tags_selected:(o:{[k: string]: string})=>void,
   LinkStroke:LinkStrokeFuncType,
   DrawArrows:drawArrowsType,
   set_display_link_opacity:(s:string)=>void,
-  contextualised_link:{ current:SankeyLink|undefined},
-  pointer_pos:{current:number[]},
   LinkSabotColor:LinkColorFuncType
 ) => JSX.Element
 
