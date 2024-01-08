6080554da8fb599013e7cc9c4f1572955ee08d32
Vincent <vincent.clavel@terriflux.fr>
2024-01-08 16:45:57 +0100

    value_editor_name_view as ref

--- packages/opensankey-plus/client/types/Types.tsx
+++ packages/opensankey-plus/client/types/Types.tsx
@@ -129,6 +129,7 @@ export interface SankeyPlusContextMenuType extends contextMenuType {
 export interface DictSetterInputValueType {
   r_setter_editor_content_fo_node:MutableRefObject<Dispatch<SetStateAction<string>> | undefined>,
   r_setter_editor_content_fo_zdt:MutableRefObject<Dispatch<SetStateAction<string>> | undefined>,
+  r_setter_value_editor_name_view:MutableRefObject<Dispatch<SetStateAction<string>> | undefined>,
 }
 export interface PlusApplicationContextType extends applicationContextType{
   has_open_sankey_plus:boolean
