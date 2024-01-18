7cdf9eb16940a53679c6ab12f98a7f7fc96b08f4
Vincent <vincent.clavel@terriflux.fr>
2024-01-18 11:07:45 +0100

    modif accordion item placement

--- opensankey/client/src/draw/SankeyDrawLinks.tsx
+++ opensankey/client/src/draw/SankeyDrawLinks.tsx
@@ -87,16 +87,10 @@ export const DrawLinks : DrawLinksFType = (
           button_ref.current.click()
         }
         if ( accordion_ref && accordion_ref.current) {
-          for ( const child in accordion_ref.current.children) {
-            if (accordion_ref.current.children[child].id === 'Flux') {
-              (accordion_ref.current.children[0] as HTMLLabelElement).click();
-              (accordion_ref.current.children[child] as HTMLLabelElement).click()
-            }
-          }
+          accordion_ref.current.click()
         }
         if ( links_accordion_ref && links_accordion_ref.current) {
-          (links_accordion_ref.current.children[0] as HTMLLabelElement).click();
-          (links_accordion_ref.current.children[1] as HTMLLabelElement).click()
+          links_accordion_ref.current.click()
         }
       }
       if(multi_selected_links.current.length>0){
