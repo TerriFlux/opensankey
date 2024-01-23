8738e283a6dc90accfbd7a52715a9d16ae7a521b
Vincent Clavel <vincent.clavel@terriflux.fr>
2024-01-23 11:36:24 +0000

    Os transform non local attr node flux to local

--- opensankey/client/src/dialogs/types/SankeyStyleTypes.tsx
+++ opensankey/client/src/dialogs/types/SankeyStyleTypes.tsx
@@ -6,10 +6,9 @@ import {
 export type SankeyModalStyleNodeFType = (
     applicationContext:applicationContextType,
     dict_variable_application_data:dict_variable_application_dataType,
-    dict_variable_elements_selected:dict_variable_elements_selectedType,
     ref_show_style_node:MutableRefObject<Dispatch<SetStateAction<boolean>>>,
     ref_selected_style_node:MutableRefObject<string>,
-    additional_node_attribute:JSX.Element[]
+    node_attribute_tab:JSX.Element[]
 ) => JSX.Element
 
 //Modal et fonctions pour l'edition et affectation des style de flux
