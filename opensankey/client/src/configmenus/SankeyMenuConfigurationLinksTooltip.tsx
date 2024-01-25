ce053f6b734bd5697dcd2072f980a7197329a062
Vincent Clavel <vincent.clavel@terriflux.fr>
2024-01-25 14:04:14 +0000

    Os branch pour ssm546

--- opensankey/client/src/configmenus/SankeyMenuConfigurationLinksTooltip.tsx
+++ opensankey/client/src/configmenus/SankeyMenuConfigurationLinksTooltip.tsx
@@ -3,6 +3,7 @@ import { Row, Form, Col, Tab, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { SankeyData, SankeyLink } from '../types/Types'
 import { TFunction } from 'i18next'
 import { MenuConfigurationLinksTooltipFType } from './types/SankeyMenuConfigurationLinksTooltipTypes'
+import { StyleTitleSubSectionMenuEditionElements } from './SankeyUtils'
 
 export const MenuConfigurationLinksTooltip : MenuConfigurationLinksTooltipFType = (
   data:SankeyData,
@@ -12,26 +13,29 @@ export const MenuConfigurationLinksTooltip : MenuConfigurationLinksTooltipFType 
   menu_for_modal:boolean
 )=>{
 
-  const content=    <Form.Group as={Row} >
-    <Col xs={12}>
-      <OverlayTrigger
-        key={'Flux.tooltips.IB'}
-        placement={'top'}
-        delay={500}
-        overlay={<Tooltip id={'Flux.tooltips.IB'}>{t('Flux.tooltips.IB')} </Tooltip>}>
-        <Form.Control
-          as="textarea"
-          rows={10}
-          value={multi_selected_links.current.length>0 && multi_selected_links.current[0].tooltip_text ? multi_selected_links.current[0].tooltip_text : ''}
-          onChange={evt => {
-            multi_selected_links.current.forEach(l=>l.tooltip_text = evt.target.value)
-            set_data({ ...data })
-          }}/>
-      </OverlayTrigger>
-    </Col>
-  </Form.Group>
+  const content=    <>
+    <h4 style={StyleTitleSubSectionMenuEditionElements({underline:true})}>{t('Noeud.IB')}</h4>
 
-  return menu_for_modal?content:<Tab key="flux_tooltip" eventKey="flux_tooltip" title={t('Flux.IB')}>
+    <Form.Group as={Row} >
+      <Col xs={12}>
+        <OverlayTrigger
+          key={'Flux.tooltips.IB'}
+          placement={'top'}
+          delay={500}
+          overlay={<Tooltip id={'Flux.tooltips.IB'}>{t('Flux.tooltips.IB')} </Tooltip>}>
+          <Form.Control
+            as="textarea"
+            rows={10}
+            value={multi_selected_links.current.length>0 && multi_selected_links.current[0].tooltip_text ? multi_selected_links.current[0].tooltip_text : ''}
+            onChange={evt => {
+              multi_selected_links.current.forEach(l=>l.tooltip_text = evt.target.value)
+              set_data({ ...data })
+            }}/>
+        </OverlayTrigger>
+      </Col>
+    </Form.Group></>
+
+  return menu_for_modal?content:<Tab key="flux_tooltip" eventKey="flux_tooltip" title={t('Flux.IS')}>
     {content}
   </Tab>
 
