03009ec937c260c6b03ddda36db1953624789785
Vincent Clavel <vincent.clavel@terriflux.fr>
2024-03-04 09:59:40 +0000

    Separateur decimale valeur flux

--- packages/opensankey/opensankey/client/src/draw/types/SankeyDrawNodesTypes.tsx
+++ packages/opensankey/opensankey/client/src/draw/types/SankeyDrawNodesTypes.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject} from 'react'
 import { GetLinkValueFuncType, LinkTextFuncType } from '../../configmenus/types/SankeyUtilsTypes'
-import { dict_variable_application_dataType, contextMenuType, dict_variable_elements_selectedType, uiElementsRefType } from '../../types/Types'
+import { dict_variable_application_dataType, contextMenuType, dict_variable_elements_selectedType, uiElementsRefType, applicationContextType } from '../../types/Types'
 import { NodeTooltipsContentFType } from './SankeyTooltipTypes'
 
 export type DrawNodesFType = (
@@ -8,6 +8,7 @@ export type DrawNodesFType = (
   dict_variable_application_data:dict_variable_application_dataType,
   uiElementsRef:uiElementsRefType,
   dict_variable_elements_selected:dict_variable_elements_selectedType,
+  applicationContext:applicationContextType,
   alt_key_pressed:MutableRefObject<boolean>,
   NodeTooltipsContent: NodeTooltipsContentFType,
   LinkText:LinkTextFuncType,
