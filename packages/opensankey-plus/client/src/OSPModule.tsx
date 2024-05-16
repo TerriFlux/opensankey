03e7027da3f4e9c8a417d3d2a0513e528ac21e2b
Julien Alapetite <julien.alapetite@terriflux.fr>
2024-05-16 07:41:00 +0200

    change import path

--- packages/opensankey-plus/client/src/OSPModule.tsx
+++ packages/opensankey-plus/client/src/OSPModule.tsx
@@ -38,7 +38,7 @@ import {
   SankeyPlusNode, 
   SankeyPlusShowMenuComponentsType,
   SankeyPlusShowMenuComponentsVarType
-} from 'sankeyanimation/types/Types'
+} from '../types/Types'
 
 import { 
   MenuDraggable, closeAllMenu, 
