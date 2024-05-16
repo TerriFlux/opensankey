f6557c59104c57f81d83667f7931e4ae6b14a2b9
Julien Alapetite <julien.alapetite@terriflux.fr>
2024-05-16 06:41:58 +0200

    remove client/src/import/SankeyIconsUtils.tsx

--- packages/opensankey-plus/client/src/import/SankeyIconsUtils.tsx
+++
@@ -1,4 +0,0 @@
-import * as SankeyIconsUtils from 'sankeyicons/dist/SankeyIconsUtils'
-import {ModalSelectionIconsType} from 'sankeyicons/src/SankeyIconsUtils'
-
-export const modal_selection_icons:ModalSelectionIconsType = SankeyIconsUtils.modal_selection_icons
