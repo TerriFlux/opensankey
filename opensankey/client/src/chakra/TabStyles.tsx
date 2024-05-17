aeb5b902b7c18daebfc17bd6e282e609fcd7a4fa
Vincent Clavel <vincent.clavel@terriflux.fr>
2024-05-17 12:47:18 +0000

    Some fix + style for card icon

--- opensankey/client/src/chakra/TabStyles.tsx
+++ opensankey/client/src/chakra/TabStyles.tsx
@@ -39,3 +39,43 @@ export const tabs_base_style = definePartsStyle({
   // tabpanel: {
   // },
 })
+
+
+export const tabs_variant_lib_cion=definePartsStyle({
+  root: {
+    border: 'inherit',
+    borderRadius: '6px',
+    borderColor: 'inherit',
+  },
+  tab: {
+    border: 'solid 0px !important',
+    borderRadius: '6px',
+    borderColor: 'none',
+    margin: '0 !important',
+    padding: '0 !important',
+    height: '2rem !important',
+    backgroundColor: ' openSankey.50',
+
+    _selected: {
+      border: 'solid 1px !important',
+      borderRadius: '6px',
+      color: 'openSankey.50',
+      borderColor: 'none',
+      backgroundColor: ' openSankey.200',
+
+    }
+  },
+  tablist: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr ',
+    gridColumnGap: '0.25rem',
+    border: '0 !important',
+    margin: '0.5rem !important'
+  },
+  tabpanels: {
+    borderTop: 'solid 1px !important',
+    borderTopColor: 'gray.100 !important',
+  },
+  // tabpanel: {
+  // },
+})
