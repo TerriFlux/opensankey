5a863d08381b01b16d89aa027f1e886639d91849
Vincent Clavel <vincent.clavel@terriflux.fr>
2024-05-17 17:10:37 +0000

    Fix detail level popover

--- packages/opensankey/opensankey/client/src/dialogs/SankeyMenuDialogs.tsx
+++ packages/opensankey/opensankey/client/src/dialogs/SankeyMenuDialogs.tsx
@@ -748,3 +748,27 @@ export const OpenSankeyDiagramSelector : OpenSankeyDiagramSelectorFType = (
   </Form>
 }
 
+// export const PopoverSelectorDetailNodes:FunctionComponent<popoverSelectorDetailNodesFType>=({
+//   applicationContext,
+//   dict_variable_application_data,
+//   applicationDraw,
+//   node_function,
+//   link_function
+// }
+// )=>{
+//   const redrawAllNodes=()=>node_function.RedrawNodes(Object.values(dict_variable_application_data.display_nodes))
+//   const redrawAllLinks=()=>link_function.RedrawLinks(Object.values(dict_variable_application_data.display_links))
+
+//   return <Popover id='popover-details-level' style={{maxWidth:'100%'}}>
+//     <Popover.Header as="h3">{applicationContext.t('Banner.ndd')}</Popover.Header>
+//     <Popover.Body style={{  marginLeft: '5px', width: '350px' }}>
+//       <>{(Object.entries(dict_variable_application_data.data.levelTags).length > 0) ? (<>
+//         {addSimpleLevelDropDown(
+//           dict_variable_application_data,applicationDraw.reDrawLegend,redrawAllNodes,redrawAllLinks,node_function.recomputeDisplayedElement
+//         )}</>
+//       ) : (<>
+//         <Form.Control placeholder="Pas de filtrage" style={{ opacity: !windowSankey.SankeyToolsStatic ? '0.3' : '0', color: '#6c757d' }} disabled /></>)}</>
+//     </Popover.Body>
+//   </Popover>
+// }
+// export default PopoverSelectorDetailNodes
