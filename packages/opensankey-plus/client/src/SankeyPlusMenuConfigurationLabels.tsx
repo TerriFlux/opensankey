a86b14b7ba2ab308167a97b3b58dd6da371f70a7
Julien Alapetite <julien.alapetite@terriflux.fr>
2024-06-04 15:41:45 +0200

    fix https://gitlab.com/su-model/opensankey/-/issues/824 remets liste de taille par défaut

--- packages/opensankey-plus/client/src/SankeyPlusMenuConfigurationLabels.tsx
+++ packages/opensankey-plus/client/src/SankeyPlusMenuConfigurationLabels.tsx
@@ -225,20 +225,20 @@ export const OSPMenuConfigurationFreeLabels : FunctionComponent<OSPMenuConfigura
   const valAllLabelBorderTransparent=IsAllZdtAttrSameValue(data,multi_selected_label.current,'transparent_border') as boolean[]
 
   // Create a custom size list of font-size
-  const list_size=[]
-  for(let i=6;i<=50;i++){
-    list_size.push(i+'px')
-  }
+  // const list_size=[]
+  // for(let i=6;i<=7;i++){
+  //   list_size.push(i+'px')
+  // }
 
-  const Size = Quill.import('attributors/style/size')
-  Size.whitelist = list_size
-  Quill.register(Size, true)
+  // const Size = Quill.import('attributors/style/size')
+  // Size.whitelist = ['small','large','huge']
+  // Quill.register(Size, true)
 
   const modules = {
     toolbar: [
       [{ 'font': [] }],
       ['bold', 'italic', 'underline','strike'],
-      [{ 'size': list_size }],
+      [{ 'size': [] }],
       [{ 'color': [] }, { 'background': [] }],
       [{'list': 'ordered'}, {'list': 'bullet'}],
       [{'align':[]}],
