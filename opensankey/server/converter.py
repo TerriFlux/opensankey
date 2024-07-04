225e6678170cecac85f87669b5acabb79e393ac5
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2024-07-04 06:53:32 +0000

    Resolve "Bug téléchargement excel"

--- opensankey/server/converter.py
+++ opensankey/server/converter.py
@@ -885,6 +885,8 @@ class SankeyToJson(object):
                 # For each node level tag group, get associated tags
                 tags = node.get_tags_from_taggroup(tagg)
                 if tags is not None:
+                    # Levels taggs are applied instead of primary
+                    node_json['dimensions']['Primaire'] = {}
                     # Get the upper level tag if it exists :
                     # ie if tag = 2, upper_tag = 1
                     # ie if tags = 3:4, upper_tag = 2
