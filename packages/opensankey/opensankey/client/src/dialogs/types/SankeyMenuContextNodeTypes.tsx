fa77978b58640a7afe2633f167cca8806a34b620
Vincent <vincent.clavel@terriflux.fr>
2024-07-17 16:16:28 +0200

    menu context (node,link,DA) implemented with classes + MenuDraggable is now functionComponent

--- packages/opensankey/opensankey/client/src/dialogs/types/SankeyMenuContextNodeTypes.tsx
+++ packages/opensankey/opensankey/client/src/dialogs/types/SankeyMenuContextNodeTypes.tsx
@@ -6,13 +6,8 @@ import {
 export type ContextMenuNodeFType = {
   applicationContext : applicationContextType,
   applicationData : applicationDataType,
-  applicationState : applicationStateType,
-  contextMenu : contextMenuType,
   dict_hook_ref_setter_show_dialog_components : dict_hook_ref_setter_show_dialog_componentsType,
   agregation: agregationType,
-  node_function:NodeFunctionTypes,
-  link_function:LinkFunctionTypes,
-  ComponentUpdater:ComponentUpdaterType,
   additional_context_element_menu:JSX.Element[],
   additional_context_element_other:JSX.Element[],
  }
