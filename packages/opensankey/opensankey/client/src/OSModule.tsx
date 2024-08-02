ec9949dd7c871e29e1895dd4ffb0b14b6b99a714
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2024-08-02 11:07:25 +0200

    Fix tag syncho layout update

--- packages/opensankey/opensankey/client/src/OSModule.tsx
+++ packages/opensankey/opensankey/client/src/OSModule.tsx
@@ -200,7 +200,7 @@ export const initializeApplicationData : initializeApplicationDataType = (
 )=>{
   const application_data = new Class_ApplicationData(false)
   if(initial_data !== undefined){
-    application_data.fromJSON(initial_data,true)
+    application_data.fromJSON(initial_data)
   }
   return {
     data : data,
