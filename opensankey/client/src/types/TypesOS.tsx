edff54961aa2f96455e3571e67113cc8b6be05a4
Vincent <vincent.clavel@terriflux.fr>
2024-09-23 11:50:54 +0200

    MenuConig init with func for override in submodule

--- opensankey/client/src/types/TypesOS.tsx
+++ opensankey/client/src/types/TypesOS.tsx
@@ -78,6 +78,10 @@ export class Class_ApplicationDataOS
   extends Class_ApplicationData<
     Class_DrawingAreaOS, Class_SankeyOS, Class_NodeElementOS, Class_LinkElementOS
   > {
+
+    public createNewMenuConfiguration(): Class_MenuConfig {
+      return new Class_MenuConfig()
+    }
   public createNewDrawingArea(id?:string): Class_DrawingAreaOS {
     const drawing_area = new Class_DrawingAreaOS(
       initial_window_height,
