2f59833c9309b271a1b37b8cdd68dc2b6570c9ca
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2024-09-30 17:42:23 +0200

    backend

--- server/views.py
+++ server/views.py
@@ -16,8 +16,7 @@ from flask import Response
 
 # ---------------------------------------------------------------
 # Local imports
-from .mailing import mail
-from .mailing import create_welcome_mail
+from .mailing import send_welcome_mail
 
 # ---------------------------------------------------------------
 # Create sankey_app app blueprint
@@ -109,14 +108,13 @@ def mail_send_welcome():
     :rtype: _type_
     """
     try:
-        msg = create_welcome_mail(
+        send_welcome_mail(
             request.json.get("email"),
             request.json.get("firstname"),
             request.json.get("lang"))
-        mail.send(msg)
     except Exception as excpt:
         response = Response(
             response='mail_send_welcome : ' + str(excpt),
-            status=402)
+            status=500)
         return response
     return Response(status=200)
