218e9884b3716df4f664d64fa48af0f2b787735f
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2024-10-01 11:19:52 +0200

    flake8

--- server/auth.py
+++ server/auth.py
@@ -268,7 +268,9 @@ def reset(token):
         try:
             user = User.verify_reset_token(token)
             if user is not None:
-                user.password = generate_password_hash(request.json.get('password'), method='sha256')
+                user.password = generate_password_hash(
+                    request.json.get('password'),
+                    method='sha256')
                 db.session.commit()
                 response['passwd_is_updated'] = True
         except Exception as excpt:
