3b768ae5ceb04e15c8d1db73d1a340c351352fdf
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2024-10-24 11:46:32 +0200

    runing serveur script local

---
+++ run_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get script dir
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ ! -f $SCRIPT_DIR/env ]; then
+    cp $SCRIPT_DIR/env.example $SCRIPT_DIR/env
+fi
+
+cd $SCRIPT_DIR
+
+source ./env
+
+sudo docker stop stripe_dev
+sudo docker run --name stripe_dev --rm -d stripe/stripe-cli --api-key $STRIPE_SECRET_KEY listen --forward-to 0.0.0.0:${INTERNAL_HTTP_PORT}/stripe/webhook
+
+python -m flask run
