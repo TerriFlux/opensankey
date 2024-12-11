63a229ed566dd5f21d258a38b7ef47dbfe1a8ed9
Alapetite <julien.alapetite@terriflux.fr>
2024-12-11 08:40:00 +0000

    fix vues

--- opensankey/client/src/dialogs/SankeyPersistence.tsx
+++ opensankey/client/src/dialogs/SankeyPersistence.tsx
@@ -449,8 +449,6 @@ export const UploadExemple: FType_UploadExemple = (
         // Clear datas & apply read datas
         new_data.drawing_area.bypass_redraws = true
         new_data.fromJSON(JSON_data as Type_JSON)
-        new_data.drawing_area.arrangeTrade(false)
-        new_data.drawing_area.draw()
       }
     })
   })
