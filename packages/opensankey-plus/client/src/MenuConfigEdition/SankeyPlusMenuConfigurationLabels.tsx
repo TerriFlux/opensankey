ae92c60199863e3095563344f51ff1439feabbff
Vincent Clavel <vincent.clavel@terriflux.fr>
2024-12-18 13:33:34 +0000

    Osp fix bug 18 12

--- packages/opensankey-plus/client/src/MenuConfigEdition/SankeyPlusMenuConfigurationLabels.tsx
+++ packages/opensankey-plus/client/src/MenuConfigEdition/SankeyPlusMenuConfigurationLabels.tsx
@@ -56,6 +56,7 @@ export const MenuPreferenceLabelsOSP: FunctionComponent<FCType_MenuPreferenceLab
   new_data_plus.menu_configuration.ref_to_checkbox_pref_container_updater.current=()=>setCount(a=>a+1)
   return <Checkbox
     ref={new_data_plus.checkbox_refs['LL']}
+    isDisabled={!new_data_plus.has_sankey_plus}
     variant='menuconfigpanel_option_checkbox'
     isChecked={new_data_plus.menu_configuration.isGivenAccordionShowed('LL')} 
     onChange={() => {
