4f08dcc65a7e0eb410a8016b0d5aa01ef4ed8f3a
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-01-25 08:19:38 +0100

    move some code in utils

--- opensankey/client/src/components/dialogs/SpreadSheet.tsx
+++ opensankey/client/src/components/dialogs/SpreadSheet.tsx
@@ -6,40 +6,10 @@ import {
   isAlphaNumericKey, isNavigationKey, keyCodes, Uncertain, UncertainCompatible, CellLocation
 } from "@silevis/reactgrid";
 import "@silevis/reactgrid/styles.css";
-import { Type_GenericApplicationData, Type_GenericDrawingArea, Type_GenericLinkElement, Type_GenericNodeElement } from '../../types/Types';
-import { ClassTemplate_Sankey } from '../../types/Sankey';
-import { ClassTemplate_NodeElement } from '../../types/Node';
-// COMPONENTS ===========================================================================
-// ! Won't work with locales using characters different than Arabic numerals (e.g. *Eastern* Arabic numerals: ١٢٣٬٤٥٦٫٧٨٩)
-// TODO: If possible add support for locales using characters different than Arabic numerals
-function getLocaleSeparators(locale: string) {
-  const testNumber = 123456.789;
-  const localeFormattedNumber = Intl.NumberFormat(locale).format(testNumber);
-
-  // Get the thousands separator of the locale
-  const thousandsSeparator = localeFormattedNumber.split("123")[1][0];
-
-  // Get the decimal separator of the locale
-  const decimalSeparator = localeFormattedNumber.split("123")[1][4];
-  return { thousandsSeparator, decimalSeparator };
-}
-
-export function parseLocaleNumber(stringNumber: string, locale = navigator.language): number {
-  if (!stringNumber.trim()) return NaN;
-  const { thousandsSeparator, decimalSeparator } = getLocaleSeparators(locale);
-  const normalizedStringNumber = stringNumber.replace(/\u00A0/g, " "); // Replace non-breaking space with normal space
-  const numberString = normalizedStringNumber
-    .replace(new RegExp(`[${thousandsSeparator}\\s]`, "g"), "") // Replace thousands separator and white-space
-    .replace(new RegExp(`\\${decimalSeparator}`, "g"), "."); // Replace decimal separator
-
-  const trimmedNumberString = numberString.replace(/^(?!-)\D+|\D+$/g, ""); // Remove characters before first and after last number, but keep negative sign
-  if (trimmedNumberString === null || trimmedNumberString.trim().length === 0) {
-    return NaN;
-  }
-  return Number(trimmedNumberString);
-}
-
-
+import { Type_GenericApplicationData, Type_GenericDrawingArea, Type_GenericLinkElement, Type_GenericNodeElement } from '../../types/Types'
+import { parseLocaleNumber} from '../../types/Utils'
+import { ClassTemplate_Sankey } from '../../types/Sankey'
+import { ClassTemplate_NodeElement } from '../../types/Node'
 
 export const SpreadSheet: FunctionComponent<{ new_data: Type_GenericApplicationData }> = (
   { new_data }: { new_data: Type_GenericApplicationData }
