75b7f7de29c7981f9c22f56bd807bce7fd50e82a
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-01-25 08:19:38 +0100

    move some code in utils

--- opensankey/client/src/types/Utils.tsx
+++ opensankey/client/src/types/Utils.tsx
@@ -350,3 +350,33 @@ export const list_palette_color = [d3.interpolateBlues, d3.interpolateBrBG, d3.i
   d3.interpolatePuBu, d3.interpolateRdBu, d3.interpolateRdGy, d3.interpolateRdYlBu, d3.interpolateRdYlGn, d3.interpolateSpectral,
   d3.interpolateTurbo, d3.interpolateViridis, d3.interpolateInferno, d3.interpolateMagma, d3.interpolatePlasma, d3.interpolateCividis,
   d3.interpolateWarm, d3.interpolateCool, d3.interpolateCubehelixDefault, d3.interpolateRainbow, d3.interpolateSinebow]
+
+// COMPONENTS ===========================================================================
+// ! Won't work with locales using characters different than Arabic numerals (e.g. *Eastern* Arabic numerals: ١٢٣٬٤٥٦٫٧٨٩)
+// TODO: If possible add support for locales using characters different than Arabic numerals
+const getLocaleSeparators = (locale: string) => {
+  const testNumber = 123456.789;
+  const localeFormattedNumber = Intl.NumberFormat(locale).format(testNumber);
+
+  // Get the thousands separator of the locale
+  const thousandsSeparator = localeFormattedNumber.split("123")[1][0];
+
+  // Get the decimal separator of the locale
+  const decimalSeparator = localeFormattedNumber.split("123")[1][4];
+  return { thousandsSeparator, decimalSeparator };
+}
+
+export const parseLocaleNumber = (stringNumber: string, locale = navigator.language) => {
+  if (!stringNumber.trim()) return NaN;
+  const { thousandsSeparator, decimalSeparator } = getLocaleSeparators(locale);
+  const normalizedStringNumber = stringNumber.replace(/\u00A0/g, " "); // Replace non-breaking space with normal space
+  const numberString = normalizedStringNumber
+    .replace(new RegExp(`[${thousandsSeparator}\\s]`, "g"), "") // Replace thousands separator and white-space
+    .replace(new RegExp(`\\${decimalSeparator}`, "g"), "."); // Replace decimal separator
+
+  const trimmedNumberString = numberString.replace(/^(?!-)\D+|\D+$/g, ""); // Remove characters before first and after last number, but keep negative sign
+  if (trimmedNumberString === null || trimmedNumberString.trim().length === 0) {
+    return NaN;
+  }
+  return Number(trimmedNumberString);
+}
