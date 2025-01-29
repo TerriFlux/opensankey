84dd8c016fd5ddb2238ad71872889687968791ca
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-01-29 18:20:17 +0100

    review welcome

--- packages/opensankey/opensankey/client/src/components/welcome/ModalWelcome.tsx
+++ packages/opensankey/opensankey/client/src/components/welcome/ModalWelcome.tsx
@@ -45,8 +45,6 @@ import resources from './resources.json'
 
 export const ModalWelcome: FunctionComponent<FCType_ModalWelcome> = ({
   new_data,
-  active_page,
-  external_pagination,
   external_content
 }) => {
   const { t } = new_data
@@ -66,15 +64,15 @@ export const ModalWelcome: FunctionComponent<FCType_ModalWelcome> = ({
     <p><b>{t('Menu.rcc_F9_bold')}</b>{t('Menu.rcc_F9')}</p>
   </>
 
-  const content_rc_not_static = <Accordion
-    // className='accordion_new_welcome'
-    defaultIndex={0}
-    allowToggle
-  >
-    {external_content['rc']}
-  </Accordion>
+  // const content_rc_not_static = <Accordion
+  //   // className='accordion_new_welcome'
+  //   defaultIndex={0}
+  //   allowToggle
+  // >
+  //   {external_content['rc']}
+  // </Accordion>
 
-  external_content['rc'] = new_data.is_static ? content_rc_static : content_rc_not_static
+  // external_content['rc'] = new_data.is_static ? content_rc_static : content_rc_not_static
 
   const content = <Modal
     variant='modal_welcome'
@@ -90,7 +88,7 @@ export const ModalWelcome: FunctionComponent<FCType_ModalWelcome> = ({
         gridTemplateColumns='minmax(10rem, 10%) auto'
         gridGap='0.25rem'
       >
-        <Breadcrumb
+        {/* <Breadcrumb
           variant={'pagination_welcome'}
           separator={''}
           listProps={{
@@ -113,7 +111,7 @@ export const ModalWelcome: FunctionComponent<FCType_ModalWelcome> = ({
                 </BreadcrumbItem>
               })
           }
-        </Breadcrumb>
+        </Breadcrumb> */}
         <Box
           display='grid'
           gridAutoFlow='rows'
@@ -129,10 +127,10 @@ export const ModalWelcome: FunctionComponent<FCType_ModalWelcome> = ({
             paddingInlineStart='1rem'
             width='calc(100% - 2rem)'
           >
-            {t('welcome.' + active_page)}
+            {t('welcome.intro')}
           </Text>
           {/* Contenu */}
-          {external_content[active_page as 'read_me' | 'intro' | 'interface' | 'rc' | 'licence' | 'news']}
+          {external_content['intro']}
         </Box>
 
       </ModalBody>
@@ -148,25 +146,23 @@ export const ModalWelcomeBuilder: FunctionComponent<FCType_ModalWelcomeBuilder> 
 ) => {
   const welcome_text = (new_data.options?.welcome_text as string) ?? ''
   const has_welcome_text = welcome_text.length > 0
-  const [active_page, set_active_page] = useState(has_welcome_text ? 'read_me' : 'intro')
-  new_data.menu_configuration.dict_setter_show_dialog.ref_setter_modal_welcome_active_page.current = set_active_page
+  //const [active_page, set_active_page] = useState(has_welcome_text ? 'read_me' : 'intro')
+  //new_data.menu_configuration.dict_setter_show_dialog.ref_setter_modal_welcome_active_page.current = set_active_page
 
-  const [page_links, page_content] = ModalWelcomeContent(
-    new_data,
-    set_active_page
+  const page_content = ModalWelcomeContent(
+    new_data
   )
 
   return <ModalWelcome
     new_data={new_data}
-    active_page={active_page}
-    external_pagination={page_links}
+    //active_page={active_page}
+    //external_pagination={page_links}
     external_content={page_content}
   />
 }
 
 export const ModalWelcomeContent = (
-  new_data: Type_GenericApplicationData,
-  set_active_page: (_: string) => void,
+  new_data: Type_GenericApplicationData
 ) => {
   const { t, static_path } = new_data
   const welcome_text = (new_data.options?.welcome_text as string) ?? ''
@@ -186,17 +182,17 @@ export const ModalWelcomeContent = (
   const page_content: { [x: string]: JSX.Element } = {}
 
   // Welcom text
-  if (has_welcome_text) {
-    page_links['read_me'] = <BreadcrumbLink onClick={() => { set_active_page('read_me') }}>
-      {t('welcome.breadcrumbs.read_me')}
-    </BreadcrumbLink>
-    page_content['read_me'] = welcome_text ? <>parse(welcome_text)</> : <></>
-  }
+  // if (has_welcome_text) {
+  //   page_links['read_me'] = <BreadcrumbLink onClick={() => { set_active_page('read_me') }}>
+  //     {t('welcome.breadcrumbs.read_me')}
+  //   </BreadcrumbLink>
+  //   page_content['read_me'] = welcome_text ? <>parse(welcome_text)</> : <></>
+  // }
 
   // Introduction to application
-  page_links['intro'] = <BreadcrumbLink onClick={() => { set_active_page('intro') }}>
-    {t('welcome.breadcrumbs.intro')}
-  </BreadcrumbLink>
+  // page_links['intro'] = <BreadcrumbLink onClick={() => { set_active_page('intro') }}>
+  //   {t('welcome.breadcrumbs.intro')}
+  // </BreadcrumbLink>
   page_content['intro'] = <div>
     {
       new_data.is_static ?
@@ -262,192 +258,189 @@ export const ModalWelcomeContent = (
   </div>
 
   // Interface explained
-  page_links['interface'] = <BreadcrumbLink onClick={() => { set_active_page('interface') }}>
-    {t('welcome.breadcrumbs.interface')}
-  </BreadcrumbLink>
-  page_content['interface'] = <>
-    <Table variant='table_welcome_buttons'>
-      <Tbody>
-        {
-          !new_data.is_static ?
-            <>
-              {/* Info account button */}
-              <Tr>
-                <Td>
-                  <Button variant={'menutop_button_goto_dashboard'}>
-                    <FaUser />
-                  </Button>
-                </Td>
-                <Td>{t('welcome.9')}</Td>
-              </Tr>
-              {/* Infos selection / edition buttons */}
-              <Tr>
-                <Td>
-                  <ButtonGroup>
-                    <Button variant={'toolbar_button_1'}>
-                      <FontAwesomeIcon icon={faArrowPointer} />
-                    </Button>
-                    <Button variant={'toolbar_button_1'}>
-                      <FontAwesomeIcon icon={faShareNodes} />
-                    </Button>
-                  </ButtonGroup>
-                </Td>
-                <Td>{t('welcome.1')}</Td>
-              </Tr>
-            </> :
-            <> </>
-        }
-        {/* Info level buttons */}
-        <Tr>
-          <Td>
-            <ButtonGroup>
-              <Button variant={'toolbar_button_2'}>
-                <FontAwesomeIcon icon={faFolderTree} />
-              </Button>
-            </ButtonGroup>
-          </Td>
-          <Td>{t('welcome.3')}</Td>
-        </Tr>
-        {/* Infos filter button */}
-        <Tr>
-          <Td>
-            <ButtonGroup>
-              <Button variant={'toolbar_button_3'}>
-                <FontAwesomeIcon icon={faSliders} />
-              </Button>
-            </ButtonGroup>
-          </Td>
-          <Td>{t('welcome.4')}</Td>
-        </Tr>
-        {/* Info resize drawing area buttons */}
-        <Tr>
-          <Td>
-            <ButtonGroup>
-              <Button variant={'toolbar_button_6'}>
-                <FontAwesomeIcon icon={faArrowsUpDown} />
-              </Button>
-              <Button variant={'toolbar_button_6'}>
-                <FontAwesomeIcon icon={faArrowsLeftRight} />
-              </Button>
-            </ButtonGroup>
-          </Td>
-          <Td>{t('welcome.5')}</Td>
-        </Tr>
-        {/* Info data choice button */}
-        <Tr>
-          <Td>
-            <ButtonGroup>
-              <Button variant={'toolbar_button_5'}>
-                <FontAwesomeIcon icon={faDiagramProject} />
-              </Button>
-            </ButtonGroup>
-          </Td>
-          <Td>{t('welcome.6')}</Td>
-        </Tr>
-        {/* Info config menu button */}
-        <Tr>
-          <Td>
-            <ButtonGroup>
-              <Button variant={'toolbar_main_button'}>
-                <FaAngleDoubleLeft />
-              </Button>
-            </ButtonGroup>
-          </Td>
-          <Td>{t('welcome.10')}</Td>
-        </Tr>
-      </Tbody>
-    </Table>
-  </>
+  // page_links['interface'] = <BreadcrumbLink onClick={() => { set_active_page('interface') }}>
+  //   {t('welcome.breadcrumbs.interface')}
+  // </BreadcrumbLink>
+  // page_content['interface'] = <>
+  //   <Table variant='table_welcome_buttons'>
+  //     <Tbody>
+  //       {
+  //         !new_data.is_static ?
+  //           <>
+  //             {/* Info account button */}
+  //             <Tr>
+  //               <Td>
+  //                 <Button variant={'menutop_button_goto_dashboard'}>
+  //                   <FaUser />
+  //                 </Button>
+  //               </Td>
+  //               <Td>{t('welcome.9')}</Td>
+  //             </Tr>
+  //             {/* Infos selection / edition buttons */}
+  //             <Tr>
+  //               <Td>
+  //                 <ButtonGroup>
+  //                   <Button variant={'toolbar_button_1'}>
+  //                     <FontAwesomeIcon icon={faArrowPointer} />
+  //                   </Button>
+  //                   <Button variant={'toolbar_button_1'}>
+  //                     <FontAwesomeIcon icon={faShareNodes} />
+  //                   </Button>
+  //                 </ButtonGroup>
+  //               </Td>
+  //               <Td>{t('welcome.1')}</Td>
+  //             </Tr>
+  //           </> :
+  //           <> </>
+  //       }
+  //       {/* Info level buttons */}
+  //       <Tr>
+  //         <Td>
+  //           <ButtonGroup>
+  //             <Button variant={'toolbar_button_2'}>
+  //               <FontAwesomeIcon icon={faFolderTree} />
+  //             </Button>
+  //           </ButtonGroup>
+  //         </Td>
+  //         <Td>{t('welcome.3')}</Td>
+  //       </Tr>
+  //       {/* Infos filter button */}
+  //       <Tr>
+  //         <Td>
+  //           <ButtonGroup>
+  //             <Button variant={'toolbar_button_3'}>
+  //               <FontAwesomeIcon icon={faSliders} />
+  //             </Button>
+  //           </ButtonGroup>
+  //         </Td>
+  //         <Td>{t('welcome.4')}</Td>
+  //       </Tr>
+  //       {/* Info resize drawing area buttons */}
+  //       <Tr>
+  //         <Td>
+  //           <ButtonGroup>
+  //             <Button variant={'toolbar_button_6'}>
+  //               <FontAwesomeIcon icon={faArrowsUpDown} />
+  //             </Button>
+  //             <Button variant={'toolbar_button_6'}>
+  //               <FontAwesomeIcon icon={faArrowsLeftRight} />
+  //             </Button>
+  //           </ButtonGroup>
+  //         </Td>
+  //         <Td>{t('welcome.5')}</Td>
+  //       </Tr>
+  //       {/* Info data choice button */}
+  //       <Tr>
+  //         <Td>
+  //           <ButtonGroup>
+  //             <Button variant={'toolbar_button_5'}>
+  //               <FontAwesomeIcon icon={faDiagramProject} />
+  //             </Button>
+  //           </ButtonGroup>
+  //         </Td>
+  //         <Td>{t('welcome.6')}</Td>
+  //       </Tr>
+  //       {/* Info config menu button */}
+  //       <Tr>
+  //         <Td>
+  //           <ButtonGroup>
+  //             <Button variant={'toolbar_main_button'}>
+  //               <FaAngleDoubleLeft />
+  //             </Button>
+  //           </ButtonGroup>
+  //         </Td>
+  //         <Td>{t('welcome.10')}</Td>
+  //       </Tr>
+  //     </Tbody>
+  //   </Table>
+  // </>
 
   // Shortcuts
-  page_links['rc'] = <BreadcrumbLink onClick={() => { set_active_page('rc') }}>
-    {t('welcome.breadcrumbs.rc')}
-  </BreadcrumbLink>
-  page_content['rc'] = <AccordionItem
-  // eventKey='OS'
-  >
-    <AccordionButton>
-      <Box
-        as='span'
-        layerStyle='menuconfig_entry'
-      >
-        {t('Menu.rcc_titre_princ')}
-      </Box>
-      <AccordionIcon />
-    </AccordionButton>
-    <AccordionPanel>
-      <Box
-        display='grid'
-        gridTemplateColumns='50% 50%'
-        gridColumnGap='0.25rem'
-        width='100%'
-        justifySelf='center'
-      >
-        <Table
-          variant='table_welcome_buttons'
-        >
-          <Thead><Th colSpan={2}>{t('Menu.rcc_titre_select')}</Th></Thead>
-          <Tbody>
-            <Tr><Td>{t('Menu.rcc_cn_bold')}</Td><Td>{t('Menu.rcc_cn')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_shift_cn_bold')}</Td><Td>{t('Menu.rcc_shift_cn')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_ctrl_cn_bold')}</Td><Td>{t('Menu.rcc_ctrl_cn')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_cf_bold')}</Td><Td>{t('Menu.rcc_cf')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_shift_cf_bold')}</Td><Td>{t('Menu.rcc_shift_cf')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_ctrl_cf_bold')}</Td><Td>{t('Menu.rcc_ctrl_cf')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_cs_bold')}</Td><Td>{t('Menu.rcc_cs')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_click_and_drag_bold')}</Td><Td>{t('Menu.rcc_click_and_drag')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_cdn_bold')}</Td><Td>{t('Menu.rcc_cdn')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_ad_bold')}</Td><Td>{t('Menu.rcc_ad')}</Td></Tr>
-          </Tbody>
-        </Table>
-        <Table
-          variant='table_welcome_buttons'
-        >
-          <Thead><Th colSpan={2}>{t('Menu.rcc_titre_edi')}</Th></Thead>
-          <Tbody>
-            <Tr><Td>{t('Menu.rcc_e_cn_bold')}</Td><Td>{t('Menu.rcc_e_cn')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_e_ds_bold')}</Td><Td>{t('Menu.rcc_e_ds')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_e_dn_bold')}</Td><Td>{t('Menu.rcc_e_dn')}</Td></Tr>
-          </Tbody>
-          <Thead><Th colSpan={2}>{t('Menu.rcc_titre_autre')}</Th></Thead>
-          <Tbody>
-            <Tr><Td>{t('Menu.rcc_a_s_bold')}</Td><Td>{t('Menu.rcc_a_s')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_a_fc_bold')}</Td><Td>{t('Menu.rcc_a_fc')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_a_dbm_bold')}</Td><Td>{t('Menu.rcc_a_dbm')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_a_ech_bold')}</Td><Td>{t('Menu.rcc_a_ech')}</Td></Tr>
-            <Tr><Td>{t('Menu.rcc_ctrl_scrll_bold')}</Td><Td>{t('Menu.rcc_ctrl_scrll')}</Td></Tr>
-          </Tbody>
-        </Table>
-      </Box>
-    </AccordionPanel>
-  </AccordionItem>
+  // page_links['rc'] = <BreadcrumbLink onClick={() => { set_active_page('rc') }}>
+  //   {t('welcome.breadcrumbs.rc')}
+  // </BreadcrumbLink>
+  // page_content['rc'] = <AccordionItem
+  // // eventKey='OS'
+  // >
+  //   <AccordionButton>
+  //     <Box
+  //       as='span'
+  //       layerStyle='menuconfig_entry'
+  //     >
+  //       {t('Menu.rcc_titre_princ')}
+  //     </Box>
+  //     <AccordionIcon />
+  //   </AccordionButton>
+  //   <AccordionPanel>
+  //     <Box
+  //       display='grid'
+  //       gridTemplateColumns='50% 50%'
+  //       gridColumnGap='0.25rem'
+  //       width='100%'
+  //       justifySelf='center'
+  //     >
+  //       <Table
+  //         variant='table_welcome_buttons'
+  //       >
+  //         <Thead><Th colSpan={2}>{t('Menu.rcc_titre_select')}</Th></Thead>
+  //         <Tbody>
+  //           <Tr><Td>{t('Menu.rcc_cn_bold')}</Td><Td>{t('Menu.rcc_cn')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_shift_cn_bold')}</Td><Td>{t('Menu.rcc_shift_cn')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_ctrl_cn_bold')}</Td><Td>{t('Menu.rcc_ctrl_cn')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_cf_bold')}</Td><Td>{t('Menu.rcc_cf')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_shift_cf_bold')}</Td><Td>{t('Menu.rcc_shift_cf')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_ctrl_cf_bold')}</Td><Td>{t('Menu.rcc_ctrl_cf')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_cs_bold')}</Td><Td>{t('Menu.rcc_cs')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_click_and_drag_bold')}</Td><Td>{t('Menu.rcc_click_and_drag')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_cdn_bold')}</Td><Td>{t('Menu.rcc_cdn')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_ad_bold')}</Td><Td>{t('Menu.rcc_ad')}</Td></Tr>
+  //         </Tbody>
+  //       </Table>
+  //       <Table
+  //         variant='table_welcome_buttons'
+  //       >
+  //         <Thead><Th colSpan={2}>{t('Menu.rcc_titre_edi')}</Th></Thead>
+  //         <Tbody>
+  //           <Tr><Td>{t('Menu.rcc_e_cn_bold')}</Td><Td>{t('Menu.rcc_e_cn')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_e_ds_bold')}</Td><Td>{t('Menu.rcc_e_ds')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_e_dn_bold')}</Td><Td>{t('Menu.rcc_e_dn')}</Td></Tr>
+  //         </Tbody>
+  //         <Thead><Th colSpan={2}>{t('Menu.rcc_titre_autre')}</Th></Thead>
+  //         <Tbody>
+  //           <Tr><Td>{t('Menu.rcc_a_s_bold')}</Td><Td>{t('Menu.rcc_a_s')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_a_fc_bold')}</Td><Td>{t('Menu.rcc_a_fc')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_a_dbm_bold')}</Td><Td>{t('Menu.rcc_a_dbm')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_a_ech_bold')}</Td><Td>{t('Menu.rcc_a_ech')}</Td></Tr>
+  //           <Tr><Td>{t('Menu.rcc_ctrl_scrll_bold')}</Td><Td>{t('Menu.rcc_ctrl_scrll')}</Td></Tr>
+  //         </Tbody>
+  //       </Table>
+  //     </Box>
+  //   </AccordionPanel>
+  // </AccordionItem>
 
-  if (!new_data.is_static) {
-    page_links['licence'] = <BreadcrumbLink onClick={() => { set_active_page('licence') }}>
-      {t('welcome.breadcrumbs.licence')}
-    </BreadcrumbLink>
-    page_content['licence'] = <Box layerStyle='welcome_license_row'>
-      <Box>
-        <img
-          src={new_data.logo_opensankey}
-          alt='logo_OS'
-          style={{ 'objectFit': 'contain', 'width': '225px' }}
-        />
-      </Box>
-      <Box layerStyle='welcome_license_desc'>
-        <span>{t('Menu.presentation_OS')}</span>
-        <Button variant='welcome_button_license_description'
-          onClick={() => {
-            window.open('https://terriflux.com/downloads/open-sankey/')
-          }}>
-          {t('contribute_to_os')}
-        </Button>
-      </Box>
-    </Box>
-  }
+  // if (!new_data.is_static) {
+  //   page_links['licence'] = <BreadcrumbLink onClick={() => { set_active_page('licence') }}>
+  //     {t('welcome.breadcrumbs.licence')}
+  //   </BreadcrumbLink>
+  //   page_content['licence'] = <Box layerStyle='welcome_license_row'>
+  //     <Box>
+  //       <img
+  //         src={new_data.logo_opensankey}
+  //         alt='logo_OS'
+  //         style={{ 'objectFit': 'contain', 'width': '225px' }}
+  //       />
+  //     </Box>
+  //     <Box layerStyle='welcome_license_desc'>
+  //       <span>{t('Menu.presentation_OS')}</span>
+  //       <Button variant='welcome_button_license_description'
+  //         onClick={() => {
+  //           window.open('https://terriflux.com/downloads/open-sankey/')
+  //         }}>
+  //         {t('contribute_to_os')}
+  //       </Button>
+  //     </Box>
+  //   </Box>
+  // }
 
-  return [
-    page_links,
-    page_content
-  ]
+  return page_content
 }
