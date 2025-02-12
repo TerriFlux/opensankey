d5d8f7c1992f3dc054f72b6ae836deefcef175ed
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2025-02-12 14:26:22 +0000

    Resolve "Fix - Templates languages + Diffuclties"

--- packages/opensankey/opensankey/client/src/types/Utils.tsx
+++ packages/opensankey/opensankey/client/src/types/Utils.tsx
@@ -1,7 +1,7 @@
 // ==================================================================================================
-// Author : Vincent LE DOZE & Vincent CLAVEL for TerriFlux SARL
+// Author : Vincent LE DOZE & Vincent CLAVEL for TerriFlux
 // Date : 29/05/2024
-// All rights reserved for TerriFlux SARL
+// All rights reserved for TerriFlux
 // ==================================================================================================
 
 // External imports
@@ -348,7 +348,7 @@ export const OSTooltip: FunctionComponent<OSTooltpFuncType> = (
       closeDelay={100}
     >
       {children}
-    </Tooltip>    
+    </Tooltip>
   }
 }
 
