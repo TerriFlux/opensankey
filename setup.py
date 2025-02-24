1fe2bedcd897ed54dc728da58e6c81676240fd2b
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-02-24 12:21:15 +0100

    setup and doc

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ import setuptools
 import os
 
 
+def get_long_description():
+    try:
+        with open('mfa_problem/README.md') as f:
+            long_description = f.read()
+            return long_description
+    except Exception:
+        with open('README.md') as f:
+            long_description = f.read()
+            return long_description
 class BuildPyCommand(setuptools.command.sdist.sdist):
     """Custom build command."""
     def run(self):
@@ -61,30 +70,33 @@ class BDistWheelInfoPyCommand(wheel.bdist_wheel.bdist_wheel):
         super(BDistWheelInfoPyCommand, self).run()
 
 
-setup(name='OpenSankey',
-      version='0.9.12',
-      description='OpenSankey',
-      url='https://gitlab.com/terriflux-public/OpenSankey',
-      author='TerriFlux',
-      author_email='contact@terriflux.fr',
-      license='MIT',
-      packages=['opensankey'],
-      package_dir={'opensankey': 'opensankey'},
-      package_data={
-            'opensankey': [
-                  'setup.cfg',
-                  'opensankey.ini',
-                  'wsgi.py',
-                  'server/*.*'
-            ]
-      },
-      cmdclass={
-          'sdist': BuildPyCommand,
-          'install': InstallPyCommand,
-          'egg_info': EggInfoPyCommand,
-          'bdist_wheel': BDistWheelInfoPyCommand
-      },
-        scripts=[
-            'opensankey/app.py'
-        ],
-      zip_safe=False)
+setup(
+    name='OpenSankey',
+    version='1.0.0b2',
+    description='OpenSankey',
+    url='https://gitlab.com/terriflux-public/OpenSankey',
+    author='TerriFlux',
+    author_email='contact@terriflux.fr',
+    license='MIT',
+    packages=[
+        'opensankey',
+        'opensankey.server'
+    ],
+    package_dir={'opensankey': 'opensankey'},
+    cmdclass={
+        'sdist': BuildPyCommand,
+        'install': InstallPyCommand,
+        'egg_info': EggInfoPyCommand,
+        'bdist_wheel': BDistWheelInfoPyCommand
+    },
+    scripts=[
+        'opensankey/app.py'
+    ],
+    zip_safe=False,
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown'
+)
+
+# git clean -d -x -f
+# python setup.py sdist bdist_wheel
+# python -m twine upload --repository pypi dist/*
