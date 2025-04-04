baf25e2c4e048ca484fe99d8d6e94e0c4689548c
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2025-04-04 12:41:00 +0200

    Small modifs on title box

--- opensankey/client/src/chakra/EditableStyle.tsx
+++ opensankey/client/src/chakra/EditableStyle.tsx
@@ -2,17 +2,17 @@
 // The MIT License (MIT)
 // ==================================================================================================
 // Copyright (c) 2025 TerriFlux
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,12 +37,11 @@ export const editable_base_style = definePartsStyle({
 })
 
 export const name_file_editable = definePartsStyle({
-
   preview: {
     display: 'block',
     alignItems: 'center',
-    paddingLeft:'0.2rem',
-    paddingRight:'0.2rem',
+    paddingLeft:'1rem',
+    paddingRight:'1rem',
     height: '2rem',
     fontSize: '1rem',
     bg: 'primaire.3',
@@ -64,7 +63,9 @@ export const name_file_editable = definePartsStyle({
     border: '1px solid',
     borderColor: 'primaire.3',
     color: 'white',
-    width: '15rem',
+    maxW: '15rem',
+    minW: '8rem',
+    width: 'unset',
     overflow: 'visible',
     // textOverflow: 'ellipsis',
     whiteSpace: 'nowrap',
