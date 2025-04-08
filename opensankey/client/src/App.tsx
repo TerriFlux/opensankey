cd2983633fddb5913b265aa49ef44abfa0a11619
Vincent LE DOZE <vincent.le-doze@terriflux.fr>
2025-04-08 09:52:14 +0200

    Linter

--- opensankey/client/src/App.tsx
+++ opensankey/client/src/App.tsx
@@ -2,17 +2,17 @@
 // The MIT License (MIT)
 // ==================================================================================================
 // Copyright (c) 2025 TerriFlux
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,8 +26,7 @@
 
 import React, {
   FunctionComponent,
-  useEffect,
-  useState
+  useEffect
 } from 'react'
 import LZString from 'lz-string'
 import * as d3 from 'd3'
@@ -47,7 +46,6 @@ import { SankeyModalStyleLink, SankeyModalStyleNode } from './components/dialogs
 import { ModalPreference } from './components/dialogs/SankeyMenuPreferences'
 
 import { Type_JSON, WrapperInitializeAdditionalMenus } from './types/Utils'
-import { Type_AdditionalMenus } from './types/Types'
 import { FCType_OpenSankeyApp } from './types/FunctionTypes'
 import { ModalDocumentation } from './components/welcome/SplashScreen'
 
