bfe3d774955794761cd318ec91563c10239d4a17
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-04-09 08:03:55 +0200

    regexp for mial

--- packages/login-component/client/src/Register/Register.tsx
+++ packages/login-component/client/src/Register/Register.tsx
@@ -34,7 +34,7 @@ import { Presentation } from './Presentation'
 import { LoginComponent } from '../LoginComponent'
 
 
-export const email_regex_str = '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]{2,4}$)'
+export const email_regex_str = '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,})$'
 export const pwd_regex_str = '^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9\n\r\t]).{8,}$'
 export const name_regex_str = '^[a-zéèêïA-Z]+([ ,.\'-][a-zéèêïA-Z]+)*[a-zéèêïA-Z]*$'
 export const lic_regex_str = '^([a-zA-Z0-9- ]{2,})$'
