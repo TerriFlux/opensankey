d21d6544936c546d65806726659d9abc4670ecda
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-05-04 07:48:23 +0200

    level tag menu

--- packages/opensankey-plus/client/src/components/MenuConfigEdition/SankeyPlusMenuConfigurationLevlTags.tsx
+++ packages/opensankey-plus/client/src/components/MenuConfigEdition/SankeyPlusMenuConfigurationLevlTags.tsx
@@ -8,7 +8,7 @@ import {
 } from '@chakra-ui/react'
 
 // Local types
-import type { Class_LevelTag, Class_Tag } from '../../deps/OpenSankey/types/Tag'
+import type { Class_LevelTag, Class_LevelTagGroup, Class_Tag } from '../../deps/OpenSankey/types/Tag'
 import type { Type_GenericApplicationData, Type_GenericNodeElement } from '../../deps/OpenSankey/types/Types'
 // import type {
 //   FCType_SankeyMenuConfigurationLevelTags
@@ -38,32 +38,35 @@ export const SankeyLevelSelectionSimple: FunctionComponent<FCType_SankeyLevelEdi
 
   // Nodes to select --------------------------------------------------------------------
 
-  // let nodes: Type_GenericNodeElement[]
-  // let selected_nodes: Type_GenericNodeElement[]
-  // if (!new_data.menu_configuration.is_selector_only_for_visible_nodes) {
-  //   // All availables nodes
-  //   nodes = new_data.drawing_area.sankey.nodes_list_sorted
-  //   selected_nodes = new_data.drawing_area.selected_nodes_list_sorted
-  // }
-  // else {
-  //   // Only visible nodes
-  //   nodes = new_data.drawing_area.sankey.visible_nodes_list_sorted
-  //   selected_nodes = new_data.drawing_area.visible_and_selected_nodes_list_sorted
-  // }
-  let selected_nodes: Class_NodeDimension[] =[]
-  //if (!new_data.menu_configuration.is_selector_only_for_visible_nodes) {
-  // All availables nodes
-  new_data.drawing_area.sankey.nodes_list.filter(n=>n.dimensions_as_parent.length> 0).forEach(n=>n.dimensions_as_parent.forEach(dim=>selected_nodes.push(dim)))
+  //let nodes: Type_GenericNodeElement[]
+  let selected_nodes: Type_GenericNodeElement[]
+  if (!new_data.menu_configuration.is_selector_only_for_visible_nodes) {
+    // All availables nodes
+    //nodes = new_data.drawing_area.sankey.nodes_list_sorted
+    selected_nodes = new_data.drawing_area.selected_nodes_list_sorted.filter(n=>n.dimensions_as_parent.length> 0)
+  }
+  else {
+    // Only visible nodes
+    //nodes = new_data.drawing_area.sankey.visible_nodes_list_sorted
+    selected_nodes = new_data.drawing_area.visible_and_selected_nodes_list_sorted.filter(n=>n.dimensions_as_parent.length> 0)
+  }
+  let selected_nodes_dimensions: Class_NodeDimension[] =[]
+  selected_nodes.forEach(n=>n.dimensions_as_parent.forEach(dim=>selected_nodes_dimensions.push(dim)))
 
-  const entries_for_nodes: typeElementSelectable = selected_nodes.map((d) => { return { 'label': d.id, 'value': d.id, selected: selected_nodes.includes(d) } })
+  const entries_for_nodes: typeElementSelectable = selected_nodes_dimensions.map((d) => { 
+    return { 
+      'value': d.id, 
+      'label': d.parent.name + '->(' + d.children.map(c=>c.name+' ')+')', selected: selected_nodes_dimensions.includes(d) 
+    } }
+  )
 
   // Menu updaters ----------------------------------------------------------------------
 
   // Boolean used to force this component to reload
   const [, setCount] = useState(0)
   // Link this menu's update function to ref
-  new_data.menu_configuration.ref_to_menu_config_nodes_selection_updater.current = () => {
-    const value_to_show = (new_data.drawing_area.selected_nodes_list.length != 1) ? '' : new_data.drawing_area.selected_nodes_list[0].name
+  new_data.menu_configuration.ref_to_menu_config_nodes_dim_selection_updater.current = () => {
+    const value_to_show = (new_data.drawing_area.selected_nodes_list.filter(n=>n.dimensions_as_parent.length> 0).length != 1) ? '' : new_data.drawing_area.selected_nodes_list.filter(n=>n.dimensions_as_parent.length> 0)[0].name
     // Update text input of node name
     ref_set_text_value_input.current(String(value_to_show))
     setCount(a => a + 1)
@@ -76,7 +79,7 @@ export const SankeyLevelSelectionSimple: FunctionComponent<FCType_SankeyLevelEdi
   const refreshThisAndToggleSaving = () => {
     // Toogle saving indicator
     new_data.menu_configuration.ref_to_save_in_cache_indicator.current(false)
-    ref_set_text_value_input.current(String((selected_nodes.length != 1) ? '' : selected_nodes[0].id))
+    ref_set_text_value_input.current(String((selected_nodes_dimensions.length != 1) ? '' : selected_nodes_dimensions[0].id))
 
     // Refresh this menu
     setCount(a => a + 1)
@@ -96,14 +99,15 @@ export const SankeyLevelSelectionSimple: FunctionComponent<FCType_SankeyLevelEdi
     onClick={(entries: typeElementSelectable) => {
       // Update selection list
       const entries_values = entries.map(d => d.value)
-      // selected_nodes.forEach(n => {
-      //   if (entries_values.includes(n.id)) {
-      //     new_data.drawing_area.addNodeToSelection(n)
-      //   }
-      //   else {
-      //     new_data.drawing_area.removeNodeFromSelection(n)
-      //   }
-      // })
+      //TODO selected nodes
+      selected_nodes_dimensions.forEach(n => {
+        if (entries_values.includes(n.id)) {
+          new_data.drawing_area.addNodeToSelection(n.parent as Type_GenericNodeElement)
+        }
+        else {
+          new_data.drawing_area.removeNodeFromSelection(n.parent as Type_GenericNodeElement)
+        }
+      })
       // Update all menus
       refreshThisAndUpdateRelatedComponents()
     }}
@@ -117,13 +121,13 @@ export const SankeyLevelSelectionSimple: FunctionComponent<FCType_SankeyLevelEdi
         layerStyle='menuconfigpanel_row_droplist_simple'
       >
 
-        {/* Liste déroulante pour selectionner un noeud */}
+        {/* Liste déroulante pour selectionner une hiérarchie de noeud */}
         <OSTooltip label={t('Menu.tooltips.noeud.slct')}>
           {/* {dropdownMultiNode()} */}
           {new_select}
         </OSTooltip>
 
-        {/* Checkbox permettant d'afficher que les noeuds visibles dans le selecteur */}
+        {/* Checkbox permettant d'afficher que les hiérarchies des noeuds visibles dans le selecteur */}
         <OSTooltip label={t('Menu.tooltips.noeud.dns')}>
           <Button
             variant='menuconfigpanel_option_button'
@@ -168,15 +172,17 @@ export const SankeyMenuConfigurationLevelTags: FunctionComponent<FCType_SankeyMe
 
   // Selected nodes ---------------------------------------------------------------------
 
-  let selected_nodes: Class_NodeDimension[] =[]
+  let selected_nodes: Type_GenericNodeElement[]
   if (!new_data.menu_configuration.is_selector_only_for_visible_nodes) {
     // All availables nodes
-    new_data.drawing_area.sankey.nodes_list.filter(n=>n.dimensions_as_parent.length> 0).forEach(n=>n.dimensions_as_parent.forEach(dim=>selected_nodes.push(dim)))
+    selected_nodes = new_data.drawing_area.selected_nodes_list_sorted.filter(n=>n.dimensions_as_parent.length> 0)
   }
   else {
     // Only visible nodes
-    //selected_nodes = new_data.drawing_area.visible_and_selected_nodes_list_sortednode.hasGivenTag(tag)
+    selected_nodes = new_data.drawing_area.visible_and_selected_nodes_list_sorted.filter(n=>n.dimensions_as_parent.length> 0)
   }
+  let selected_nodes_dimensions: Class_NodeDimension[] =[]
+  selected_nodes.forEach(n=>n.dimensions_as_parent.forEach(dim=>selected_nodes_dimensions.push(dim)))
 
   // Menu updaters ----------------------------------------------------------------------
 
@@ -191,7 +197,7 @@ export const SankeyMenuConfigurationLevelTags: FunctionComponent<FCType_SankeyMe
     setCount(a => a + 1)
 
   }
-  //new_data.menu_configuration.ref_to_menu_config_level_tags_updater.current = updateThis
+  new_data.menu_configuration.ref_to_menu_config_nodes_dim_tags_updater.current = updateThis
 
 
   // Utils functions --------------------------------------------------------------------
@@ -202,65 +208,40 @@ export const SankeyMenuConfigurationLevelTags: FunctionComponent<FCType_SankeyMe
    * @return [allTrue: boolean, allFalse: boolean]
    */
   const haveAllSelectedLevelGivenTag = (
-    tag: Class_LevelTag
+    tagg: Class_LevelTagGroup
   ) => {
     let allTrue = true
     let allFalse = true
-    // selected_nodes
-    //   .forEach(node => {
-    //     const test = true//node.hasGivenTag(tag)
-    //     allTrue = allTrue && (test === true)
-    //     allFalse = allFalse && (test === false)
-    //   })
+    selected_nodes_dimensions
+      .forEach(node_dim => {
+        const test = node_dim.parent_level_tag.group == tagg
+        allTrue = allTrue && (test === true)
+        allFalse = allFalse && (test === false)
+      })
     return [allTrue, allFalse]
   }
 
   // JSX content ------------------------------------------------------------------------
 
   // Return nothing if there is no tag or no nodes are selected
-  // if (!has_level_taggs || selected_nodes.length==0)
-  //   return <></>
+  if (!has_level_taggs || selected_nodes.length==0)
+    return <></>
 
   const content = <>
-    {/* <Box
-      as='span'
-      layerStyle='menu_sub_section_title'>
-      {t('Menu.level_associated_tag')}
-    </Box> */}
     <Box layerStyle='menuconfigpanel_grid' >
-      {/* Groupe d'étiquettes  */}
-      <Select
-        isDisabled={!new_data.has_sankey_plus}
-        variant='menuconfigpanel_option_select'
-        value={level_tagg_entry_index}
-        onChange={(evt: React.ChangeEvent<HTMLSelectElement>) =>
-          setNodeTaggEntryIndex(Number(evt.target.value))
-        }
-      >
-        {
-          list_level_taggs
-            .map((level_tagg, level_tagg_index) =>
-              <option
-                key={level_tagg.id}
-                value={level_tagg_index}
-              >
-                {level_tagg.name}
-              </option>
-            )
-        }
-      </Select>
-      {/* {has_level_taggs ? <Box
+    <SankeyLevelSelectionSimple new_data={new_data} />
+      {<Box
         layerStyle='menuconfigpanel_grid'
       >
         {
-          level_tagg_entry.tags_list
-            .map(level_tag => {
-              const [allTrue, allFalse] = haveAllSelectedLevelGivenTag(level_tag)
+          list_level_taggs
+            .map(level_tagg => {
+              const [allTrue, allFalse] = haveAllSelectedLevelGivenTag(level_tagg)
               return <Checkbox
                 isDisabled={!new_data.has_sankey_plus}
                 variant='menuconfigpanel_tag_checkbox'
                 isIndeterminate={
-                  (selected_nodes.length > 1) &&
+                  (selected_nodes_dimensions.length > 1) &&
                   (!allTrue) &&
                   (!allFalse)
                 }
@@ -270,14 +251,14 @@ export const SankeyMenuConfigurationLevelTags: FunctionComponent<FCType_SankeyMe
                   //new_data.drawing_area.updateSelectedLevelTagAssignation(visible, level_tag)
                 }}
               >
-                {level_tag.name}
+                {level_tagg.name}
               </Checkbox>
             })
         }
-      </Box> : <></>} */}
+      </Box>}
     </Box>
-    <SankeyLevelSelectionSimple new_data={new_data} />
   </>
+
   return <WrapperBoxSubSectionMenu new_data={new_data} title={t('Menu.level_associated_tag')}>
     <OSTooltip label={new_data.has_sankey_plus ? '' : t('Menu.sankeyOSPDisabled')}>
       {content}
