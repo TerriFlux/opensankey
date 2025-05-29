a50df3b5b8033bc09ac1f1e2d80543c9956259ca
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-05-29 12:48:18 +0200

    script

--- build_server.bat
+++ build_server.bat
@@ -2,6 +2,7 @@
 setlocal enabledelayedexpansion
 
 REM Étape 1 : Installation des requirements
+echo Install OpenSankey requirements
 powershell -Command "pip install -r requirements.txt | Select-String -NotMatch 'Requirement already satisfied'"
 powershell -Command "pip install -r conda_requirements.txt | Select-String -NotMatch 'Requirement already satisfied'"
 
@@ -11,10 +12,13 @@ call build.bat
 popd
 
 REM Étape 3 : Vérification flake8
+echo OpenSankey flake8
 pushd opensankey\server
 flake8
 popd
 
 REM Étape 4 : Installation du package principal
+echo OpenSankey install
 pip install .
+echo OpenSankey done
 
