8e9b08b821aa3ca54db3e8a8a17dc271b1bcb78b
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-05-29 10:35:58 +0200

    git clean

--- git_clean.bat
+++ git_clean.bat
@@ -1,9 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Fichier à exclure
+set EXCLUDE_FILE=server\db.sqlite
+
 echo Nettoyage du repo principal
 git reset --hard
-git clean -dxf
+git clean -dxf -e %EXCLUDE_FILE%
 
 echo Nettoyage récursif des sous-modules
 git submodule foreach --recursive "git reset --hard && git clean -dxf"
