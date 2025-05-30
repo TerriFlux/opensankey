d06da0dc1be5adc7a5ffcb9bc0c5bd9189dfbfe7
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-05-30 18:53:18 +0200

    scripts generate tests

--- generate_tests_references.bat
+++ generate_tests_references.bat
@@ -7,19 +7,17 @@ REM ========================================
 
 chcp 65001 >nul
 
-REM === Vérifier si la variable d'environnement TESTS_DIR est définie ===
-IF NOT DEFINED TESTS_DIR (
-    REM Définir TESTS_DIR au dossier .\Tests relatif à l’emplacement actuel
-    SET "TESTS_DIR=%CD%\..\\Tests"
-    echo [INFO] TEST_DIR non défini. Défini à %TEST_DIR%
+
+
+rem === Install deps ===
+for %%S in (SankeyExcelParser) do (
+    echo %%S generate_tests_references.bat
+    pushd submodules\%%S
+    call generate_tests_references.bat
+    popd
 )
 
-REM === Vérifier que le dossier TESTS_DIR existe ===
-IF NOT EXIST "%TESTS_DIR%" (
-    echo [ERREUR] Le dossier TESTS_DIR n'existe pas : "%TESTS_DIR%"
-    exit /b 1
-)
-
+echo Generate Tests references in OpenSankey
 REM === Lancer le script Python avec les arguments transmis au .bat ===
 python opensankey\tests\test_dict_results.py --generate_results %*
 
