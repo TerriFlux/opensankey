52cda2b265b0696713e4cae34320e21a4dda4b4d
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-06-16 12:08:20 +0200

    fix logo

--- client/src/types/ApplicationDataSA.tsx
+++ client/src/types/ApplicationDataSA.tsx
@@ -49,14 +49,6 @@ export class Class_ApplicationDataSA extends Class_ApplicationDataOSP {
   public get menu_configuration(): Class_MenuConfigSA { return this._menu_configuration }
   public set menu_configuration(_: Class_MenuConfigSA) { this._menu_configuration = _ }
 
-  // Overrride logo
-  public get logo() {
-    if (!this._has_sankey_plus)
-      return this.logo_opensankey
-    else
-      return this.logo_sankey_plus
-  }
-
   public get icon_library(): Class_IconLibrarySA { return this._icon_library as Class_IconLibrarySA }
 
 }
