f9a53f1124bd47c20d90a2ad6459c69750a3c4ad
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-08-26 10:21:20 +0200

    maj

--- opensankey/client/src/types/Tag.tsx
+++ opensankey/client/src/types/Tag.tsx
@@ -721,16 +721,16 @@ export class Class_LevelTag {
   // PUBLIC METHODES ====================================================================
 
   public update() {
-    this.dimensions_list_as_tag_for_children.forEach(dim => {
-      if (dim.parent_level_tag.group.activated) {
-        dim.showAccordingToLevelTags()
-      }
-    })
-    this.dimensions_list_as_tag_for_parent.forEach(dim => {
-      if (dim.parent_level_tag.group.activated) {
-        dim.showAccordingToLevelTags()
-      }
-    })
+    // this.dimensions_list_as_tag_for_children.forEach(dim => {
+    //   if (dim.parent_level_tag.group.activated) {
+    //     dim.showAccordingToLevelTags()
+    //   }
+    // })
+    // this.dimensions_list_as_tag_for_parent.forEach(dim => {
+    //   if (dim.parent_level_tag.group.activated) {
+    //     dim.showAccordingToLevelTags()
+    //   }
+    // })
   }
 
   public setSelected() {
