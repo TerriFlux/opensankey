60c6664f39351719ee243c9c5defecd38f5b1952
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-10-08 11:21:39 +0200

    fix typescript errors

--- packages/opensankey-plus/client/src/traductions/traduction.tsx
+++ packages/opensankey-plus/client/src/traductions/traduction.tsx
@@ -2,13 +2,16 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-import { deep_assign_resources, resources_opensankey,use_context_config } from '../deps/OpenSankey/traductions/traduction'
+import { deep_assign_resources, resources_opensankey, use_context_config, I18nResources } from '../deps/OpenSankey/traductions/traduction'
 import { resources_app_elements } from './traduction_app_elements'
 import { resources_template } from './traduction_templates'
-import { createZDDMenuConfigPlus,createNodeMenuConfigPlus } from '../components/ContextMenuConfigs'
+import { createZDDMenuConfigPlus, createNodeMenuConfigPlus } from '../components/ContextMenuConfigs'
+import { MenuConfig } from '../deps/OpenSankey/components/dialogs/SankeyMenuContext'
 
-use_context_config(resources_opensankey,createZDDMenuConfigPlus(),'ContextMenuZDD')
-use_context_config(resources_opensankey,createNodeMenuConfigPlus(),'ContextMenuNodes')
+//@ts-expect-error xxx
+use_context_config(resources_opensankey as unknown as I18nResources, createZDDMenuConfigPlus(), 'ContextMenuZDD')
+//@ts-expect-error xxx
+use_context_config(resources_opensankey as unknown as I18nResources, createNodeMenuConfigPlus(), 'ContextMenuNodes')
 // Traduction increment for OpenSankey+
 export const resources_opensankeyplus = {}
 deep_assign_resources(resources_opensankey, resources_opensankeyplus)
