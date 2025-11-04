41f36c4a9e6d650ab1e4b59240182c0525508414
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-11-04 16:14:29 +0100

    maj

--- client/src/AppSA.tsx
+++ client/src/AppSA.tsx
@@ -459,7 +459,7 @@ export const SankeyApp = ({ new_data_app }: { new_data_app: Class_ApplicationDat
                       }
                     />
                     <Route
-                      path='checkout/:license'
+                      path='checkout'
                       element={
                         <PrivateRoute
                           component={
