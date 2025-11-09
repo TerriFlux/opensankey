426143e5657e576faa47a215beeecf61807a62c9
Julien Alapetite <julien.alapetite@terriflux.fr>
2025-11-09 18:06:03 +0100

    maj

--- opensankey/client/src/Elements/NodeDrawShape.tsx
+++ opensankey/client/src/Elements/NodeDrawShape.tsx
@@ -136,14 +136,12 @@ export class NodeDrawShape {
         .filter(tag => tag.is_selected)
       
       if (selected_tags_for_colormap.length > 0 ) {
-        // if a node has several tags we take the first one. The logic is given
-        // by the following example. Meuble en hêtre has two tags hêtre and feuillu
-        // we put hêtre first as it is the most desagregated. This way we can display
-        // the nodes with different colors depending of the level of detail selected.
         shape_color = selected_tags_for_colormap[0].color
       } else {
         shape_color = default_element_color
       }
+    } else {
+      shape_color = default_element_color
     }
     
     return shape_color
