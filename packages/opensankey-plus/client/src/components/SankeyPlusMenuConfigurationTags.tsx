9fee18a6eaf1e5451f4dbf4462d352aa36f075af
Julien Alapetite <julien.alapetite@terriflux.fr>
2026-02-01 18:27:46 +0100

    maj

--- packages/opensankey-plus/client/src/components/SankeyPlusMenuConfigurationTags.tsx
+++ packages/opensankey-plus/client/src/components/SankeyPlusMenuConfigurationTags.tsx
@@ -891,13 +891,6 @@ const SankeySettingsEditionElementTags: FC<FType_SankeySettingsEditionElementTag
                         >
                           {t('Tags.Multiple')}
                         </option>:<></>
-                        {/* <option
-                          key={'unitary' + tag_group.id}
-                          id='OneBaner'
-                          value='unitary'
-                        >
-                          {'Unitary'}
-                        </option> */}
                         {elementTagNameProp == 'data_taggs' ? <option
                           key={'sequence' + tag_group.id}
                           id='SequenceBaner'
--- packages/opensankey-plus/client/src/components/UnitaryBoard.tsx
+++ packages/opensankey-plus/client/src/components/UnitaryBoard.tsx
@@ -1,10 +1,11 @@
 import { updateUnitaryStyles } from "../deps/OpenSankey/Algorithms/UnitaryBoard"
-import { elementStyleConfigs, LinkInUnitaryStyle, LinkOutUnitaryStyle, node_unitary_styles, SankeyUnitaryNodeInputStyle, SankeyUnitaryNodeOutputStyle, SankeyUnitaryNodeStyle } from "../deps/OpenSankey/Elements/ElementStyle"
+import { elementStyleConfigs, node_unitary_styles } from "../deps/OpenSankey/Elements/ElementStyle"
 import { Class_NodeElement } from "../deps/OpenSankey/Elements/Node"
 import { DrawingAreaPersistence } from "../deps/OpenSankey/Persistence/SankeyPersistence"
 import { compressJSONToGzip } from "../deps/OpenSankey/Persistence/UniversalJSONCompression"
 import { Class_DrawingArea } from "../deps/OpenSankey/types/DrawingArea"
 import { Class_Tag } from "../deps/OpenSankey/types/Tag"
+import { Class_ViewTagGroup } from "../deps/OpenSankey/types/TagGroup"
 import { makeId } from "../deps/OpenSankey/types/Utils"
 import { Class_ApplicationDataOSP } from "../types/ApplicationDataOSP"
 
@@ -12,90 +13,197 @@ import { Class_ApplicationDataOSP } from "../types/ApplicationDataOSP"
  * Crée une vue unitaire - soit un board avec tous les nœuds, soit une vue focalisée sur un nœud spécifique
  * @param app_data - L'application data
  * @param node_ref - Le nœud de référence (optionnel). Si fourni, crée une vue focalisée sur ce nœud
- * @param is_board - Si true, crée un board avec tous les nœuds (ignoré si node_ref est fourni)
  */
 export const createUnitaryView = (
-    app_data: Class_ApplicationDataOSP,
-    node_ref?: Class_NodeElement
+  app_data: Class_ApplicationDataOSP,
+  node_ref?: Class_NodeElement
 ) => {
-    // If no base sankey is given, we take the currently active sankey
-    const base_drawing_area = app_data.drawing_area
-    base_drawing_area.purgeSelection()
+  // If no base sankey is given, we take the currently active sankey
+  const base_drawing_area = app_data.drawing_area
+  base_drawing_area.purgeSelection()
 
-    // If no view existed previously, we add the active sankey as master sankey
-    if (!app_data.has_views && !app_data.master_drawing_area) {
-        app_data.master_drawing_area = app_data.drawing_area
-        app_data.drawing_area.sankey.setInvisible()
-        app_data.drawing_area.purgeSelection()
-        app_data.drawing_area.unDraw()
-    }
+  // If no view existed previously, we add the active sankey as master sankey
+  if (!app_data.has_views && !app_data.master_drawing_area) {
+    app_data.master_drawing_area = app_data.drawing_area
+    app_data.drawing_area.sankey.setInvisible()
+    app_data.drawing_area.purgeSelection()
+    app_data.drawing_area.unDraw()
+  }
 
-    // Create the new sankey
-    const new_drawing_area = app_data.createNewDrawingArea(makeId('unitary_view'))
-    new_drawing_area.is_unitary = true
-    new_drawing_area.bypass_redraws = true
+  // Create the new sankey
+  const new_drawing_area = app_data.createNewDrawingArea(makeId('unitary_view'))
+  new_drawing_area.is_unitary = true
+  new_drawing_area.bypass_redraws = true
 
-    // Copy current sankey
-    const name = node_ref
-        ? 'Unitary view of ' + node_ref.name
-        : 'Board Unitary View'
-    const id = new_drawing_area.id
-    const copy = DrawingAreaPersistence.toJSON(base_drawing_area, { keep_siblings: true })
-    copy.id = id
-    DrawingAreaPersistence.fromJSON(new_drawing_area, copy)
-    new_drawing_area.name = name
+  // Copy current sankey
+  const name = node_ref ? 'Unitary view of ' + node_ref.name : 'Board Unitary View'
+  const id = new_drawing_area.id
+  const copy = DrawingAreaPersistence.toJSON(base_drawing_area, { keep_siblings: true })
+  copy.id = id
+  DrawingAreaPersistence.fromJSON(new_drawing_area, copy)
+  new_drawing_area.name = name
 
-    // Supprimer les containers
-    new_drawing_area.sankey.containers_list.forEach(cont => {
-        new_drawing_area.deleteContainer(cont)
-    })
+  // Supprimer les containers
+  new_drawing_area.sankey.containers_list.forEach(cont => {
+    new_drawing_area.deleteContainer(cont)
+  })
 
-    // Créer les styles unitaires
-    node_unitary_styles.forEach(style_id =>
-        new_drawing_area.sankey.create_internal_style(style_id, elementStyleConfigs)
+  // Créer les styles unitaires
+  node_unitary_styles.forEach(style_id =>
+    new_drawing_area.sankey.create_internal_style(style_id, elementStyleConfigs)
+  )
+
+  // Récupérer les tags de type de nœud existants
+  const node_type = new_drawing_area.sankey.node_taggs_dict['type de noeud']
+  const echangeTag = node_type?.tags_dict['echange']
+  const productTag = node_type?.tags_dict['produit']
+  const sectorTag = node_type?.tags_dict['secteur']
+
+  // Filtrer les nœuds par type
+  const products_nodes = new_drawing_area.sankey.nodes_list.filter(n => n.hasGivenTag(productTag))
+  const sector_nodes = new_drawing_area.sankey.nodes_list.filter(n => n.hasGivenTag(sectorTag))
+  const other_nodes = new_drawing_area.sankey.nodes_list.filter(n => 
+    !n.hasGivenTag(echangeTag) && 
+    !n.hasGivenTag(productTag) && 
+    !n.hasGivenTag(sectorTag)
+  )
+
+  // Créer les ViewTagGroup et établir les relations
+  let unitary_tags_groups: Class_ViewTagGroup[] = []
+
+  if (node_type && (sectorTag || productTag)) {
+    // Cas avec distinction secteurs/produits
+    const sector_unitary_tag_group = new_drawing_area.sankey.addViewTagGroup(
+      'sector_unitary',
+      'Sankey Unitaire Secteurs'
     )
+    unitary_tags_groups.push(sector_unitary_tag_group)
 
-    // Créer le tag group unitaire
-    const unitary_tag_group = new_drawing_area.sankey.addNodeTagGroup('unitary', 'Sankey Unitaire', false)
-    unitary_tag_group.banner = 'one'
+    const product_unitary_tag_group = new_drawing_area.sankey.addViewTagGroup(
+      'product_unitary',
+      'Sankey Unitaire Produits'
+    )
+    unitary_tags_groups.push(product_unitary_tag_group)
 
-    const echangeTag = new_drawing_area.sankey.node_taggs_dict['type de noeud']?.tags_dict['echange']
-    const other_nodes = new_drawing_area.sankey.nodes_list.filter(n => !n.hasGivenTag(echangeTag))
+    // Établir la relation de sibling entre les deux groupes
+    sector_unitary_tag_group.addSibling(product_unitary_tag_group)
 
-    // Créer un tag pour chaque nœud non-échange
-    other_nodes.forEach(n => {
-        const tag = unitary_tag_group.addTag(n.name)
-        n.addTag(tag as Class_Tag)
-        // Si on crée un board, tous les tags sont désélectionnés
-        // Si on crée une vue focalisée, seul le tag du node_ref sera sélectionné
-        tag.is_selected = n.id == node_ref?.id ? true : false
+    // Déterminer quel groupe activer selon le node_ref
+    let activate_sectors = true
+    if (node_ref) {
+      if (node_ref.hasGivenTag(productTag)) {
+        activate_sectors = false
+      } else if (node_ref.hasGivenTag(sectorTag)) {
+        activate_sectors = true
+      }
+    }
+
+    // Activer/désactiver les groupes
+    sector_unitary_tag_group.activated = activate_sectors
+    product_unitary_tag_group.activated = !activate_sectors
+
+    // Ajouter les tags pour les secteurs
+    sector_nodes.forEach(n => {
+      const tag = sector_unitary_tag_group.addTag(n.name)
+      n.addTag(tag as Class_Tag)
+      // Si on crée une vue focalisée, seul le tag du node_ref est sélectionné
+      // Si on crée un board, le premier tag est sélectionné par défaut
+      if (node_ref) {
+        tag.is_selected = n.id === node_ref.id
+      } else {
+        tag.is_selected = false
+      }
     })
 
-    // Configuration commune
-    new_drawing_area.removeMinimumLinkThickness()
-    new_drawing_area.removeMaximumLinkThickness()
-    new_drawing_area.filter_label = 0
-    new_drawing_area.filter_link_value = 0
+    // Ajouter les tags pour les produits
+    products_nodes.forEach(n => {
+      const tag = product_unitary_tag_group.addTag(n.name)
+      n.addTag(tag as Class_Tag)
+      if (node_ref) {
+        tag.is_selected = n.id === node_ref.id
+      } else {
+        tag.is_selected = false
+      }
+    })
 
-    updateUnitaryStyles(new_drawing_area)
-    new_drawing_area.draw()
-    new_drawing_area.to_recenter = true
-    new_drawing_area.recenter()
-    new_drawing_area.unDraw()
-    app_data.views_dict[new_drawing_area.id] = {
-        'name': new_drawing_area.name,
-        'json': compressJSONToGzip(DrawingAreaPersistence.toJSON(new_drawing_area))
+    // S'assurer qu'au moins un tag est sélectionné dans le groupe actif
+    const active_group = activate_sectors ? sector_unitary_tag_group : product_unitary_tag_group
+    if (active_group.selected_tags_list.length === 0 && active_group.tags_list.length > 0) {
+      active_group.tags_list[0].is_selected = true
     }
-    app_data.pushViewIdInViewOrder(new_drawing_area.id)
 
-    return new_drawing_area
+  } else {
+    // Cas simple : un seul groupe pour tous les nœuds
+    const unitary_tag_group = new_drawing_area.sankey.addViewTagGroup(
+      'unitary',
+      'Sankey Unitaire'
+    )
+    unitary_tags_groups.push(unitary_tag_group)
+    unitary_tag_group.activated = true
+
+    // Ajouter les tags pour tous les nœuds (hors échange)
+    other_nodes.forEach(n => {
+      const tag = unitary_tag_group.addTag(n.name)
+      n.addTag(tag as Class_Tag)
+      if (node_ref) {
+        tag.is_selected = n.id === node_ref.id
+      } else {
+        tag.is_selected = false
+      }
+    })
+
+    // S'assurer qu'au moins un tag est sélectionné
+    if (unitary_tag_group.selected_tags_list.length === 0 && unitary_tag_group.tags_list.length > 0) {
+      unitary_tag_group.tags_list[0].is_selected = true
+    }
+  }
+
+  // Configurer tous les groupes avec banner 'one'
+  unitary_tags_groups.forEach(tagGroup => {
+    tagGroup.banner = 'one'
+  })
+
+  // Configuration commune
+  new_drawing_area.removeMinimumLinkThickness()
+  new_drawing_area.removeMaximumLinkThickness()
+  new_drawing_area.filter_label = 0
+  new_drawing_area.filter_link_value = 0
+
+  // Appliquer les styles unitaires
+  updateUnitaryStyles(new_drawing_area)
+
+  // Dessiner et centrer
+  new_drawing_area.draw()
+  new_drawing_area.to_recenter = true
+  new_drawing_area.recenter()
+  new_drawing_area.unDraw()
+
+  // Sauvegarder la vue
+  app_data.views_dict[new_drawing_area.id] = {
+    'name': new_drawing_area.name,
+    'json': compressJSONToGzip(DrawingAreaPersistence.toJSON(new_drawing_area))
+  }
+  app_data.pushViewIdInViewOrder(new_drawing_area.id)
+
+  return new_drawing_area
 }
 
-// Fonctions de compatibilité pour garder l'ancienne API
+/**
+ * Crée un board unitaire avec tous les nœuds visibles
+ * @param app_data - L'application data
+ */
 export const createUnitaryBoard = (app_data: Class_ApplicationDataOSP) => {
-    return createUnitaryView(app_data, undefined)
+  return createUnitaryView(app_data, undefined)
 }
 
-export const createUnitaryNewView = (app_data: Class_ApplicationDataOSP, node_ref: Class_NodeElement) => {
-    return createUnitaryView(app_data, node_ref)
+/**
+ * Crée une vue unitaire focalisée sur un nœud spécifique
+ * @param app_data - L'application data
+ * @param node_ref - Le nœud de référence
+ */
+export const createUnitaryNewView = (
+  app_data: Class_ApplicationDataOSP,
+  node_ref: Class_NodeElement
+) => {
+  return createUnitaryView(app_data, node_ref)
 }
