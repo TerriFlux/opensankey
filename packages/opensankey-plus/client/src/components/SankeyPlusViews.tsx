1126e4431e9221a40d74752820f9270563dfd541
Julien Alapetite <julien.alapetite@terriflux.fr>
2026-03-18 19:13:40 +0100

    maj

--- packages/opensankey-plus/client/src/components/SankeyPlusViews.tsx
+++ packages/opensankey-plus/client/src/components/SankeyPlusViews.tsx
@@ -568,18 +568,6 @@ export const BannerViewsOSP = ({ app_data }: { app_data: Class_ApplicationDataOS
  * CTA button shown in navbar when user does not have OpenSankey+ license.
  * Redirects to /license/checkout.
  */
-export const BannerSubscriptionOSP = ({ app_data, onClickSubscribe }: { app_data: Class_ApplicationDataOSP, onClickSubscribe: () => void }) => {
-  const { t } = app_data
-  if (app_data.has_sankey_plus || app_data.is_static) return <></>
-  return <OSTooltip label={t('Menu.get_premium_tooltip')} placement='bottom'>
-    <Button
-      variant='button_banner_subscription'
-      onClick={onClickSubscribe}
-    >
-      {t('Menu.get_premium')}
-    </Button>
-  </OSTooltip>
-}
 
 /**
  * View selector for navbar or menuconfig
