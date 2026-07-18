73e26824107b83de3b019e0948025f7cfc8c4327
Alexandre <alexandre.pannier@terriflux.fr>
2026-07-18 20:12:46 +0200

    chore(os): corriger l'indentation eslint de ViewsConfig.tsx (reverdir lint:ci)
    Le job `build` de main échouait sur `lint:ci` d'opensankey-plus/client : 96
    erreurs `indent` dans src/components/views/ViewsConfig.tsx (indentation à +2
    espaces), introduites par un commit récent. `eslint --fix` seul, aucune
    modification de logique. lint:ci repasse à 0 erreur.

--- packages/opensankey-plus/client/src/components/views/ViewsConfig.tsx
+++ packages/opensankey-plus/client/src/components/views/ViewsConfig.tsx
@@ -70,100 +70,100 @@ export const ViewsConfig = (
   }
   return <Box layerStyle='menuconfigpanel_grid' sx={compact_sx}>
 
-      <Box as='span' layerStyle='menuconfigpanel_row_2cols' >
-        <Box layerStyle='menuconfigpanel_option_name' >
-          {t('view.select')}
-        </Box>
-        <InputGroup
-          variant='menuconfigpanel_option_input'>
-          <SelecteurView app_data={app_data} />
-        </InputGroup>
+    <Box as='span' layerStyle='menuconfigpanel_row_2cols' >
+      <Box layerStyle='menuconfigpanel_option_name' >
+        {t('view.select')}
       </Box>
-      <Box as='span' layerStyle='menuconfigpanel_row_2cols'>
-        <Box layerStyle='menuconfigpanel_option_name'>
-          {t('view.show_master_in_list')}
-        </Box>
-        <Checkbox
-          variant='menuconfigpanel_option_checkbox'
-          isDisabled={!is_activated}
-          isChecked={app_data.show_master_in_views}
-          onChange={() => {
-            app_data.show_master_in_views = !app_data.show_master_in_views
-            menu_configuration_osp.updateComponentRelatedToViews()
-            refreshThis()
-          }}
-        />
-      </Box>
-      <Table variant='table_view' size='sm'>
-        <Thead>
-          <Tr>
-            <Th>{t('view.name')}</Th>
-            <Th>Position</Th>
-            <Th>{t('view.delete')}</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {list_view.map((view_id, idx) => {
-            const is_master = view_id == default_main_sankey_id
-            return (
-              <React.Fragment key={idx}>
-                <Tr style={{ 'border': (view_id === curr_view.id) ? '2px solid #5a9282' : 'none' }}>
-                  <Td>
-                    <Input
-                      variant='menuconfigpanel_option_input'
-                      // Maître : libellé éditable dédié (master_view_name), placeholder = défaut.
-                      value={is_master ? app_data.master_view_name : app_data.views_dict[view_id].name}
-                      placeholder={is_master ? t('view.actual') : undefined}
-                      isDisabled={!is_activated}
-                      onChange={evt => {
-                        if (is_master) app_data.master_view_name = evt.target.value
-                        else app_data.views_dict[view_id].name = evt.target.value
-                        refreshThis()
-                      }}
-                      onBlur={() => {
-                        menu_configuration_osp.updateComponentRelatedToViews()
-                      }}
-                    />
-                  </Td>
-                  <Td>
-                    {/* Change the position of the view in the liste of view from master data */}
-                    <Button variant='menuconfigpanel_option_button_in_table' isDisabled={!is_activated || (view_id == default_main_sankey_id)}
-                      onClick={() => { app_data.moveViewUpInOrder(view_id); menu_configuration_osp.updateComponentRelatedToViews() }}
-                    >
-                      {icon_move_element_up}
-                    </Button>
-                    <Button variant='menuconfigpanel_option_button_in_table' isDisabled={!is_activated || (view_id == default_main_sankey_id)}
-                      onClick={() => { app_data.moveViewDownInOrder(view_id); menu_configuration_osp.updateComponentRelatedToViews() }}
-                    >
-                      {icon_move_element_down}
-                    </Button>
-                  </Td>
-                  <Td>
-                    <Button
-                      variant='menuconfigpanel_del_button_in_table'
-                      isDisabled={!is_activated || (view_id == default_main_sankey_id)}
-                      onClick={
-                        // Delete the view. Une vue emporte toute sa géométrie propre :
-                        // snapshot avant/après (les vues sont dans le _toJSON OSP).
-                        () => {
-                          app_data.runWithSnapshotUndo(
-                            () => {
-                              app_data.deleteView(view_id)
-                              menu_configuration_osp.updateComponentRelatedToViews()
-                            },
-                            () => menu_configuration_osp.updateComponentRelatedToViews()
-                          )
-                        }
-                      }
-                    >
-                      {icon_remove_element}
-                    </Button>
-                  </Td>
-                </Tr>
-              </React.Fragment>
-            )
-          })}
-        </Tbody>
-      </Table>
+      <InputGroup
+        variant='menuconfigpanel_option_input'>
+        <SelecteurView app_data={app_data} />
+      </InputGroup>
     </Box>
+    <Box as='span' layerStyle='menuconfigpanel_row_2cols'>
+      <Box layerStyle='menuconfigpanel_option_name'>
+        {t('view.show_master_in_list')}
+      </Box>
+      <Checkbox
+        variant='menuconfigpanel_option_checkbox'
+        isDisabled={!is_activated}
+        isChecked={app_data.show_master_in_views}
+        onChange={() => {
+          app_data.show_master_in_views = !app_data.show_master_in_views
+          menu_configuration_osp.updateComponentRelatedToViews()
+          refreshThis()
+        }}
+      />
+    </Box>
+    <Table variant='table_view' size='sm'>
+      <Thead>
+        <Tr>
+          <Th>{t('view.name')}</Th>
+          <Th>Position</Th>
+          <Th>{t('view.delete')}</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {list_view.map((view_id, idx) => {
+          const is_master = view_id == default_main_sankey_id
+          return (
+            <React.Fragment key={idx}>
+              <Tr style={{ 'border': (view_id === curr_view.id) ? '2px solid #5a9282' : 'none' }}>
+                <Td>
+                  <Input
+                    variant='menuconfigpanel_option_input'
+                    // Maître : libellé éditable dédié (master_view_name), placeholder = défaut.
+                    value={is_master ? app_data.master_view_name : app_data.views_dict[view_id].name}
+                    placeholder={is_master ? t('view.actual') : undefined}
+                    isDisabled={!is_activated}
+                    onChange={evt => {
+                      if (is_master) app_data.master_view_name = evt.target.value
+                      else app_data.views_dict[view_id].name = evt.target.value
+                      refreshThis()
+                    }}
+                    onBlur={() => {
+                      menu_configuration_osp.updateComponentRelatedToViews()
+                    }}
+                  />
+                </Td>
+                <Td>
+                  {/* Change the position of the view in the liste of view from master data */}
+                  <Button variant='menuconfigpanel_option_button_in_table' isDisabled={!is_activated || (view_id == default_main_sankey_id)}
+                    onClick={() => { app_data.moveViewUpInOrder(view_id); menu_configuration_osp.updateComponentRelatedToViews() }}
+                  >
+                    {icon_move_element_up}
+                  </Button>
+                  <Button variant='menuconfigpanel_option_button_in_table' isDisabled={!is_activated || (view_id == default_main_sankey_id)}
+                    onClick={() => { app_data.moveViewDownInOrder(view_id); menu_configuration_osp.updateComponentRelatedToViews() }}
+                  >
+                    {icon_move_element_down}
+                  </Button>
+                </Td>
+                <Td>
+                  <Button
+                    variant='menuconfigpanel_del_button_in_table'
+                    isDisabled={!is_activated || (view_id == default_main_sankey_id)}
+                    onClick={
+                      // Delete the view. Une vue emporte toute sa géométrie propre :
+                      // snapshot avant/après (les vues sont dans le _toJSON OSP).
+                      () => {
+                        app_data.runWithSnapshotUndo(
+                          () => {
+                            app_data.deleteView(view_id)
+                            menu_configuration_osp.updateComponentRelatedToViews()
+                          },
+                          () => menu_configuration_osp.updateComponentRelatedToViews()
+                        )
+                      }
+                    }
+                  >
+                    {icon_remove_element}
+                  </Button>
+                </Td>
+              </Tr>
+            </React.Fragment>
+          )
+        })}
+      </Tbody>
+    </Table>
+  </Box>
 }
